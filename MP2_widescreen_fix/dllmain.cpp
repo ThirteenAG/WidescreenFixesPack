8408f9aec0cea68617fbadc1bf2edfa12bcb4d54
ThirteenAG <thirteenag@gmail.com>
2014-07-08 23:58:07 +0400

    Widescreen Fixes Pack v1.4
    - Max Payne 2 bugfix

--- MP2_widescreen_fix/dllmain.cpp
+++ MP2_widescreen_fix/dllmain.cpp
@@ -257,7 +257,7 @@ DWORD WINAPI Thread(LPVOID)
 				Sleep(1);
 				if ((unsigned char)*(DWORD*)nComicsCheck == 0xAE)
 				{
-					stdViewPortSize = 640.0f * 1.17936117936f;
+					stdViewPortSize = 640.0f * (aspect_ratio / 1.50740740741f);
 					CPatch::SetFloat((DWORD)h_e2mfc_dll + 0x434F4, 480.0f / 1.17936117936f); //480.0f
 				}
 				else
