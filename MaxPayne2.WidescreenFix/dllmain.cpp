1448831c1c14f8a87ff8f28d309656b42398197d
ThirteenAG <thirteenag@gmail.com>
2015-10-30 23:49:34 +0300

    WFP v4.0.8
    - MP2 fix update

--- MaxPayne2.WidescreenFix/dllmain.cpp
+++ MaxPayne2.WidescreenFix/dllmain.cpp
@@ -1,75 +1,98 @@
 #include "..\includes\stdafx.h"
-#include "..\includes\CPatch.h"
 
-HMODULE h_e2mfc_dll = NULL;
-HMODULE comics = NULL;
+struct Screen
+{
+	int nWidth;
+	int nHeight;
+	float fWidth;
+	float fHeight;
+	float fDynamicScreenFieldOfViewScale;
+	float fFieldOfView;
+	float fAspectRatio;
+	float fGlobalScale;
+	float fHudScale;
+	float fHudOffset;
+	float fHudOffsetWide;
+} Screen;
 
-float iniFOV, FOV;
-float fAspectRatio, stdViewPortSize, fWidthFactor, fDiffFactor;
+struct TextCoords
+{
+	float a;
+	float b;
+	float c;
+	float d;
+};
+
+union HudPos
+{
+	DWORD dwPos;
+	float fPos;
+} HudScaleX, HudScaleY;
+
+float f1_480 = 1.0f / 480.0f;
+
+HMODULE e2mfc, e2_d3d8_driver_mfc;
+float fFOVFactor;
+bool bComicsMode, bFixHud, bWidescreenHud;
+float fWidthScale, fHalfWidthScale, f1_fWidthScale;
+float fDouble1_fWidthScale, fHalf1_fWidthScale;
+uchar* IsInComicsMode;
+DWORD GetScreenRectJmp;
+DWORD jmpAddr, jmpAddr2, jmpAddr3, jmpAddr4, jmpAddr5;
+DWORD jmpAddr6, jmpAddr7, jmpAddr8, jmpAddr9;
+DWORD nCounter;
+float* pHudElementPosX; float* pHudElementPosY; TextCoords* pTextElementPosX;
+float fMirrorFactor;
 float fVisibilityFactor1, fVisibilityFactor2;
-unsigned int ScreenWidth, ScreenHeight;
-DWORD jmpAddress, jmpFOV;
-DWORD _EAX, _EBX, _nEDX; float _EDX;
-bool bComicsMode, bMenuFix;
+float fViewPortSizeX = 640.0f, fViewPortSizeY = 480.0f;
+float fFullscreen2DScale;
+int nCutsceneBorders;
 
-int __fastcall PDriverGetWidth(int a1)
+int __fastcall PDriverGetWidth(int _this)
 {
-	int result; // eax@2
-
-	if (*(DWORD *)(a1 + 48))
-		result = *(DWORD *)(a1 + 52);
+	if (*(DWORD *)(_this + 48))
+		Screen.nWidth = *(DWORD *)(_this + 52);
 	else
-		result = *(DWORD *)(a1 + 4);
+		Screen.nWidth = *(DWORD *)(_this + 4);
 
-	ScreenWidth = result;
-	return result;
+	return Screen.nWidth;
 }
 
-int __fastcall PDriverGetHeight(int a1)
+int __fastcall PDriverGetHeight(int _this)
 {
-	int result; // eax@2
-
-	if (*(DWORD *)(a1 + 48))
-		result = *(DWORD *)(a1 + 56);
+	if (*(DWORD *)(_this + 48))
+		Screen.nHeight = *(DWORD *)(_this + 56);
 	else
-		result = *(DWORD *)(a1 + 8);
+		Screen.nHeight = *(DWORD *)(_this + 8);
 
-	ScreenHeight = result;
-	return result;
+	return Screen.nHeight;
 }
 
-void __declspec(naked)PatchFOV()
+void __fastcall P_CameraSetFOVHook(int _this, float a2)
 {
-	__asm mov ebx, FOV
-	__asm push    ebx //FOV value
-	__asm mov     ecx, esi
-	__asm call    ebp
-	__asm jmp jmpFOV
-}
-
-void __fastcall P_CameraSetFOV(DWORD P_Camera, float FOVvalue)
-{
-	FOVvalue *= iniFOV;
-
-	if (*(float *)&FOVvalue != *(float *)(P_Camera + 284))
+	if (a2 != *(float *)(_this + 284))
 	{
-		if (*(float *)&FOVvalue >= 0.0)
+		if (a2 >= 0.0)
 		{
-		
-			if (FOVvalue <= 3.1241393f)
-			{
-				*(BYTE *)(P_Camera + 236) |= 0x40u;
-				*(float *)(P_Camera + 284) = FOVvalue;
-			}
+			Screen.fFieldOfView = 3.1241393f;
+			if (a2 <= 3.1241393)
+				Screen.fFieldOfView = a2 * Screen.fDynamicScreenFieldOfViewScale;
+			*(BYTE *)(_this + 236) |= 0x40u;
+			*(float *)(_this + 284) = Screen.fFieldOfView;
 		}
 		else
 		{
-			*(BYTE *)(P_Camera + 236) |= 0x40u;
-			*(DWORD *)(P_Camera + 284) = 0;
+			*(BYTE *)(_this + 236) |= 0x40u;
+			*(DWORD *)(_this + 284) = 0;
 		}
 	}
 }
 
+void __fastcall FOVHook(DWORD *_this, float a2)
+{
+	Screen.fFieldOfView = a2 * Screen.fDynamicScreenFieldOfViewScale;
+	*((float *)_this + 334) = a2;
+}
 
 void __declspec(naked)ForceEntireViewport()
 {
@@ -82,159 +105,432 @@ void __declspec(naked)DisableSubViewport()
 	__asm ret 10h
 }
 
+DWORD _EAX, _EBX, _nEDX; float _EDX, fDiffFactor;
 void __declspec(naked)PCameraValidateHook()
 {
 	__asm mov _EAX, eax
 	__asm mov _EBX, ebx
-	__asm mov     edx, [esp + 4]
+	__asm mov edx, [esp + 4]
 	__asm mov _EDX, edx
 	__asm mov _nEDX, edx
-
-	if (!fDiffFactor)
+	
+	if (_EDX >= 0.7f && _EDX <= 0.8f)
 	{
-		fDiffFactor = fWidthFactor / _EDX;
+		if ((_EBX == 1 && _EAX == 8) || (_EBX == 0 && _EAX != 8))
+		{
+			_EDX *= fDiffFactor;
+		}
 	}
 	else
 	{
-		if (_EDX >= 0.7f && _EDX <= 0.8f)
+		if (_EAX == 8 && _EDX < 0.7f)
 		{
-			if ((_EBX == 1 && _EAX == 8) || (_EBX == 0 && _EAX != 8) || (_EAX == 8 && _nEDX == 0x3F3340CF && bMenuFix == 1))
-			{
+			if (_EBX == 1 && _nEDX != 0x3ED413CD) //0.414213568, it's to avoid comics stretching
 				_EDX *= fDiffFactor;
-				__asm mov edx, _EDX
+		}
+		else
+		{
+			if ((_EDX > 0.8f/* && _EDX <= 1.5f*/) && _nEDX != 0x3F800000)
+			{
+				if ((_EBX == 1 && _EAX == 8) || (_EBX == 0 && _EAX != 8))
+					_EDX *= fDiffFactor;
+			}
+		}
+	}
+
+	__asm mov edx, _EDX
+	__asm mov[esi + 20Ch], edx
+	__asm jmp jmpAddr
+}
+
+DWORD WINAPI ComicsHandler(LPVOID)
+{
+	CIniReader iniReader("");
+	bComicsMode = iniReader.ReadInteger("MAIN", "ComicsMode", 1) != 0;
+	DWORD nComicsModeKey = iniReader.ReadInteger("MAIN", "ComicsModeKey", VK_F2);
+	bool bPatched;
+
+	injector::WriteMemory((DWORD)e2mfc + 0x176D4 + 0x2, &fViewPortSizeX, true);
+	injector::WriteMemory((DWORD)e2mfc + 0x17719 + 0x2, &fViewPortSizeY, true);
+	injector::WriteMemory((DWORD)e2mfc + 0x1779A + 0x2, &fViewPortSizeY, true);
+
+	while (true)
+	{
+		Sleep(0);
+
+		if ((GetAsyncKeyState(nComicsModeKey) & 1) && (*IsInComicsMode == 0xAE))
+		{
+			bComicsMode = !bComicsMode;
+			bPatched = !bPatched;
+			iniReader.WriteInteger("MAIN", "ComicsMode", bComicsMode);
+			while ((GetAsyncKeyState(nComicsModeKey) & 0x8000) > 0) { Sleep(0); }
+		}
+
+		if (bComicsMode)
+		{
+			if (*IsInComicsMode == 0xAE)
+			{
+				if (!bPatched)
+				{
+					fViewPortSizeX = 480.0f * Screen.fAspectRatio;
+					fViewPortSizeY = 480.0f;
+					bPatched = true;
+				}
+			}
+			else
+			{
+				if (bPatched)
+				{
+					fViewPortSizeX = 640.0f;
+					fViewPortSizeY = 480.0f;
+					bPatched = false;
+				}
 			}
 		}
 		else
 		{
-			if (_EAX == 8 && _EDX < 0.7f)
+			if (*IsInComicsMode == 0xAE)
 			{
-				_EDX *= fDiffFactor;
-				if (_EBX == 1 && _EAX == 8 && _nEDX != 0x3ED413CD) //0.414213568, it's to avoid comics stretching
-					__asm mov edx, _EDX
+				if (!bPatched)
+				{
+					fViewPortSizeX = (480.0f * Screen.fAspectRatio) / 1.17936117936f;
+					fViewPortSizeY = 480.0f / 1.17936117936f;
+					bPatched = true;
+				}
 			}
 			else
 			{
-				if ((_EDX > 0.8f/* && _EDX <= 1.5f*/) && _nEDX != 0x3F800000)
+				if (bPatched)
 				{
-					_EDX *= fDiffFactor;
-					if ((_EBX == 1 && _EAX == 8) || (_EBX == 0 && _EAX != 8))
-						__asm mov edx, _EDX
+					fViewPortSizeX = 480.0f * Screen.fAspectRatio;
+					fViewPortSizeY = 480.0f;
+					bPatched = false;
 				}
 			}
 		}
 	}
-	__asm mov[esi + 20Ch], edx
-	__asm jmp jmpAddress
+	return 0;
 }
+
+float ElementPosX, ElementNewPosX1, ElementNewPosX2;
+float ElementPosY, ElementNewPosY1, ElementNewPosY2;
+DWORD __EAX, __ESP20, __ESP2C, __ESP3C, __ESP34, __ESP44;
+float __ECX;
+float* f10042294;
+float f10042B58 = 0.003125f;
+float f10042B50 = -1.0f;
+bool bDoFix;
+void __declspec(naked) P_HudPosHook2()
+{
+	__asm mov __EAX, eax
+
+	__asm mov eax, [esp + 2Ch]
+	__asm mov __ESP2C, eax
+
+	__asm mov eax, [esp+3Ch]
+	__asm mov __ESP3C, eax
+
+	__asm mov eax, [esp + 44h]
+	__asm mov __ESP44, eax
+
+	__asm mov eax, __EAX
+		
+	bDoFix = false;
+
+	if ((__ESP44 != 0x43F00000 && __ESP2C == 0x459C4400 || __ESP2C == 0x40400000) && (__ESP3C != 0x43000000 && __ESP3C != 0x42800000)) //bullet time fullscreen effect and player's shadow 
+	{
+		bDoFix = true;
+		_asm
+		{
+			fmul    dword ptr ds : Screen.fHudScale
+			jmp	    jmpAddr4
+		}
+	}
+
+	_asm
+	{
+		fmul    ds : f10042B58
+		jmp	    jmpAddr4
+	}
+}
+
+void __declspec(naked) P_HudPosHook3()
+{
+	if (bDoFix) 
+	{
+		_asm
+		{
+			fmul    dword ptr ds : Screen.fHudOffset
+			jmp	    jmpAddr5
+		}
+	}
+
+	_asm
+	{
+		fmul    ds : f10042B50
+		jmp	    jmpAddr5
+	}
+}
+
+void __declspec(naked) P_HudPosHook()
+{
+	__asm mov __EAX, eax
+	__asm mov __ECX, ecx
+	__asm mov __ESP20, esp
+	__asm mov __ESP34, esp
+	__ESP20 += 0x20;
+	__ESP34 += 0x34;
+
+	ElementPosX = *pHudElementPosX;
+	ElementPosY = *pHudElementPosY;
+	ElementNewPosX1 = ElementPosX;
+	ElementNewPosY1 = ElementPosY;
+	ElementNewPosX2 = ElementPosX;
+	ElementNewPosY2 = ElementPosY;
+
+	if (bWidescreenHud)
+	{
+		if (ElementPosX == 7.0f) // bullet time meter
+		{
+			ElementNewPosX1 = ElementPosX + Screen.fHudOffsetWide;
+		}
+
+		if (ElementPosX == 8.0f) // bullet time overlay()
+		{
+			ElementNewPosX1 = ElementPosX + Screen.fHudOffsetWide;
+		}
+
+		if (ElementPosX == 12.0f) // painkillers
+		{
+			ElementNewPosX1 = ElementPosX + Screen.fHudOffsetWide;
+		}
+
+		if (ElementPosX == 22.5f) //health bar and overlay
+		{
+			ElementNewPosX1 = ElementPosX + Screen.fHudOffsetWide;
+		}
+
+		if (ElementPosX == 96.0f) // other weapons name
+		{
+			ElementNewPosX1 = ElementPosX - Screen.fHudOffsetWide;
+		}
+
+		if (ElementPosX == 192.0f) //molotovs/grenades name pos
+		{
+			ElementNewPosX1 = ElementPosX - Screen.fHudOffsetWide;
+		}
+	}
+
+	ElementNewPosX2 = ElementNewPosX1;
+
+	if (bDoFix && ElementPosX == 0.0f && ElementPosY == 0.0f && __EAX == 2 && __ECX == 640.0f && *(DWORD*)__ESP20 != 0x40400000) // fading
+	{
+		ElementNewPosX1 = ElementPosX + (Screen.fHudOffsetWide + fFullscreen2DScale);
+		ElementNewPosX2 = ElementPosX - (Screen.fHudOffsetWide + fFullscreen2DScale);					  
+		ElementNewPosY1 = ElementPosY + (Screen.fHudOffsetWide + fFullscreen2DScale);
+		ElementNewPosY2 = ElementPosY - (Screen.fHudOffsetWide + fFullscreen2DScale);
+	}
+	
+	_asm
+	{
+		fld     ds:f10042294
+		mov     eax, [esi+164h]
+		fsub    dword ptr[ElementNewPosX1]
+		mov     [esp+44h], eax
+		sub     esp, 8
+		fstp    dword ptr [esp+30h]
+		fld     dword ptr [esp+1Ch]
+		fsub    dword ptr[ElementNewPosX2]
+		fstp    dword ptr [esp+1Ch]
+		fld     ds:f10042294
+		fsub    dword ptr[ElementNewPosY1]
+		fstp    dword ptr [esp+2Ch]
+		fld     dword ptr [esp+20h]
+		fsub    dword ptr[ElementNewPosY2]
+		jmp	    jmpAddr3
+	}
+}
+
+DWORD pTempPtr;
+float TextPosX, TextNewPosX, TextUnkVal;
+void __declspec(naked) P_TextPosHook()
+{
+	__asm mov eax, [esp + 0xC]
+	__asm mov eax, [eax + 0x5C]
+	__asm mov TextUnkVal, eax
+
+	TextPosX = pTextElementPosX->a;
+	TextNewPosX = TextPosX;
+
+	_asm pushad
+	_asm pushfd
+
+	if ((pTextElementPosX->a == 0.0f || pTextElementPosX->a == -8.0f || pTextElementPosX->a == -16.0f || pTextElementPosX->a == -24.0f || pTextElementPosX->a == -32.0f) && pTextElementPosX->b == -10.5f && (pTextElementPosX->c == 8.0f || pTextElementPosX->c == 16.0f || pTextElementPosX->c == 24.0f || pTextElementPosX->c == 32.0f) && pTextElementPosX->d == 21) //ammo numbers(position depends on digits amount)
+	{
+		if (TextUnkVal < 0.0f)
+		{
+			TextNewPosX = TextPosX - Screen.fHudOffsetWide;
+		}
+		else
+		{
+			TextNewPosX = TextPosX + Screen.fHudOffsetWide;
+		}
+	}
+
+	_asm popfd
+	_asm popad
+
+	__asm fld    dword ptr[TextNewPosX]
+	__asm jmp    jmpAddr6
+}
+
+float __fastcall GetBordersSize(int _this)
+{
+	if (nCutsceneBorders > 1)
+	{
+		return *(float *)(*(DWORD *)_this + 4692) * (1.0f / ((4.0f / 3.0f) / Screen.fAspectRatio));
+	}
+	else
+	{
+		return 1.0f;
+	}
+}
+
+void __declspec(naked) CWndCreateExHook()
+{
+	_asm
+	{
+		mov     eax, WS_VISIBLE | WS_POPUP
+		mov     [ebp - 10h], eax
+		jmp     jmpAddr7
+	}
+}
+
 
 DWORD WINAPI Thread(LPVOID)
 {
+	injector::MakeJMP((DWORD)GetModuleHandle("mfc71.dll") + 0x12184, CWndCreateExHook, true); //fullscreen mode border fix for win10
+	jmpAddr7 = (DWORD)GetModuleHandle("mfc71.dll") + 0x12184 + 0x6;
+
 	CIniReader iniReader("");
-	iniFOV = iniReader.ReadFloat("MAIN", "FOV", 0.0f);
-	bMenuFix = (iniReader.ReadInteger("MAIN", "MenuFix", 1));
+	bool bUseGameFolderForSavegames = iniReader.ReadInteger("MAIN", "UseGameFolderForSavegames", 0) != 0;
+	if (bUseGameFolderForSavegames)
+		injector::WriteMemory<uchar>(0x41D14C, 0x85, true);
+
+	bool bAltTab = iniReader.ReadInteger("MAIN", "AllowAltTabbingWithoutPausing", 0) != 0;
+	if (bAltTab)
+		injector::MakeNOP(0x404935, 5, true);
+
+	nCutsceneBorders = iniReader.ReadInteger("MAIN", "CutsceneBorders", 1);
+	if (nCutsceneBorders)
+		injector::MakeJMP(0x41EF70, GetBordersSize, true);
+
+	fFOVFactor = iniReader.ReadFloat("MAIN", "FOVFactor", 0.0f);
+	bFixHud = iniReader.ReadInteger("MAIN", "FixHud", 1) != 0;
+	bWidescreenHud = iniReader.ReadInteger("MAIN", "WidescreenHud", 1) != 0;
+	float fWidescreenHudOffset = iniReader.ReadFloat("MAIN", "WidescreenHudOffset", 100.0f);
+	fFullscreen2DScale = iniReader.ReadFloat("MAIN", "Fullscreen2DScale", 10.0f);
+	if (!fWidescreenHudOffset) { fWidescreenHudOffset = 100.0f; }
+	if (!fFullscreen2DScale) { fFullscreen2DScale = 10.0f; }
 
 	do
 	{
 		Sleep(100);
-		h_e2mfc_dll = GetModuleHandle("e2mfc.dll");
-	} while (h_e2mfc_dll == NULL);
+		e2mfc = GetModuleHandle("e2mfc.dll");
+	} while (e2mfc == NULL);
 
-	CPatch::RedirectCall((DWORD)h_e2mfc_dll + 0x176AF, PDriverGetWidth);
-	CPatch::RedirectCall((DWORD)h_e2mfc_dll + 0x176F4, PDriverGetHeight);
-
-	CPatch::RedirectJump((DWORD)h_e2mfc_dll + 0x156A0, DisableSubViewport);
-	CPatch::RedirectJump((DWORD)h_e2mfc_dll + 0x14C80, ForceEntireViewport);
+	injector::MakeCALL((DWORD)e2mfc + 0x176AF, PDriverGetWidth, true);
+	injector::MakeCALL((DWORD)e2mfc + 0x176F4, PDriverGetHeight, true);
 
 	do
 	{
 		Sleep(0);
-	} while (ScreenWidth == 0 || ScreenHeight == 0);
+	} while (Screen.nWidth == 0 || Screen.nHeight == 0);
 
-	/*if (iniFOV)
+	Screen.fWidth = static_cast<float>(Screen.nWidth);
+	Screen.fHeight = static_cast<float>(Screen.nHeight);
+	Screen.fAspectRatio = Screen.fWidth / Screen.fHeight;
+	Screen.fDynamicScreenFieldOfViewScale = fFOVFactor;
+	if (fFOVFactor)
+		injector::MakeJMP((DWORD)GetProcAddress(e2mfc, "?setFOV@P_Camera@@QAEXM@Z"), P_CameraSetFOVHook, true);
+
+	IsInComicsMode = (uchar*)((DWORD)e2mfc + 0x61183);
+
+	if (Screen.fAspectRatio > (4.0f / 3.0f))
 	{
-		//FOV = iniFOV;
-		//jmpFOV = 0x428F86;
-		//CPatch::RedirectJump(0x428F81, PatchFOV); //fov hack
-		//CPatch::RedirectJump((DWORD)h_e2mfc_dll + 0x14E70, P_CameraSetFOV);
-		//CPatch::SetPointer(0x428F7B, P_CameraSetFOV);
-	}*/
+		fWidthScale = 640.0f / Screen.fAspectRatio;
+		fHalfWidthScale = fWidthScale / 2.0f;
+		f1_fWidthScale = 1.0f / fWidthScale;
+		fDouble1_fWidthScale = f1_fWidthScale * 2.0f;
+		fHalf1_fWidthScale = f1_fWidthScale / 2.0f;
 
-	fAspectRatio = static_cast<float>(ScreenWidth) / static_cast<float>(ScreenHeight);
+		injector::MakeJMP((DWORD)e2mfc + 0x156A0, DisableSubViewport, true);
+		injector::MakeJMP((DWORD)e2mfc + 0x14C80, ForceEntireViewport, true);
 
-	//fWidthFactor = fAspectRatio / (16.0f / 9.0f);
-	fWidthFactor = 0.7673270702f / ((4.0f / 3.0f) / fAspectRatio);
-
-	if (fAspectRatio >= 1.4f)
-	{
-		jmpAddress = (DWORD)h_e2mfc_dll + 0x15041;
-		CPatch::RedirectJump((DWORD)h_e2mfc_dll + 0x15037, PCameraValidateHook);
+		fDiffFactor = 1.0f / ((4.0f / 3.0f) / (Screen.fAspectRatio));
+		injector::MakeJMP((DWORD)e2mfc + 0x15037, PCameraValidateHook, true);
+		jmpAddr = (DWORD)e2mfc + 0x15041;
 
 		//object disappearance fix
-		CPatch::SetFloat((DWORD)h_e2mfc_dll + 0x15B87 + 0x6, 0.0f);
+		injector::WriteMemory<float>((DWORD)e2mfc + 0x15B87 + 0x6, 0.0f, true);
 
-		stdViewPortSize = 640.0f;
-		CPatch::SetPointer((DWORD)h_e2mfc_dll + 0x176D4 + 0x2, &stdViewPortSize);
+		do
+		{
+			Sleep(100);
+		} while (GetModuleHandle("X_GameObjectsMFC.dll") == NULL);
 
-		DWORD nComicsCheck = (DWORD)h_e2mfc_dll + 0x61183;
+		//mirrors fix
+		fMirrorFactor = 1.0f * ((4.0f / 3.0f) / (Screen.fAspectRatio));
+		injector::WriteMemory((DWORD)GetModuleHandle("X_GameObjectsMFC.dll") + 0x101F39 + 0x2, &fMirrorFactor, true);
+		jmpAddr2 = (DWORD)e2mfc + 0x17155 + 0x6;
 
 		//doors graphics fix
 		fVisibilityFactor1 = 0.5f;
 		fVisibilityFactor2 = 1.5f;
-		//CPatch::SetPointer((DWORD)GetModuleHandle("X_GameObjectsMFC.dll") + 0xA8D0 + 0x4F5 + 0x2, &fVisibilityFactor4);
-		CPatch::SetPointer((DWORD)GetModuleHandle("X_GameObjectsMFC.dll") + 0xA8D0 + 0x4CE + 0x2, &fVisibilityFactor1);
-		//CPatch::SetPointer((DWORD)GetModuleHandle("X_GameObjectsMFC.dll") + 0xA8D0 + 0x4AC + 0x2, &fVisibilityFactor3);
-		CPatch::SetPointer((DWORD)GetModuleHandle("X_GameObjectsMFC.dll") + 0xA8D0 + 0x485 + 0x2, &fVisibilityFactor2);
+		injector::WriteMemory((DWORD)GetModuleHandle("X_GameObjectsMFC.dll") + 0xA8D0 + 0x4CE + 0x2, &fVisibilityFactor1, true);
+		injector::WriteMemory((DWORD)GetModuleHandle("X_GameObjectsMFC.dll") + 0xA8D0 + 0x485 + 0x2, &fVisibilityFactor2, true);
 
-		//CPatch::SetPointer((DWORD)GetModuleHandle("X_GameObjectsMFC.dll") + 0x101F39 + 0x2, &fVisibilityFactor1);
-		//CPatch::SetPointer((DWORD)GetModuleHandle("X_GameObjectsMFC.dll") + 0x101F67 + 0x2, &fVisibilityFactor2);
-
-		if (iniReader.ReadInteger("MAIN", "ComicsMode", 0))
-			bComicsMode = !bComicsMode;
-
-		while (true)
+		if (bFixHud)
 		{
-			Sleep(0);
 
-			if ((GetAsyncKeyState(VK_F2) & 1) && ((unsigned char)*(DWORD*)nComicsCheck == 0xAE))
-			{
-				bComicsMode = !bComicsMode;
-				iniReader.WriteInteger("MAIN", "ComicsMode", bComicsMode);
-				while ((GetAsyncKeyState(VK_F2) & 0x8000) > 0) { Sleep(0); }
-			}
 
-			if (bComicsMode)
+			Screen.fHudOffset = ((-1.0f / Screen.fAspectRatio) * (4.0f / 3.0f));
+			Screen.fHudScale = (1.0f / (480.0f * Screen.fAspectRatio)) * 2.0f;
+			injector::MakeJMP((DWORD)e2mfc + 0xF011, P_HudPosHook2, true);
+			jmpAddr4 = (DWORD)e2mfc + 0xF011 + 0x6;
+			injector::MakeJMP((DWORD)e2mfc + 0xF05E, P_HudPosHook3, true);
+			jmpAddr5 = (DWORD)e2mfc + 0xF05E + 0x6;
+
+			injector::WriteMemory<float>((DWORD)e2mfc + 0x42B50, Screen.fHudOffset, true);
+			injector::WriteMemory<float>((DWORD)e2mfc + 0x42B58, Screen.fHudScale, true);
+
+			pHudElementPosX = (float*)((DWORD)e2mfc + 0x61134);
+			pHudElementPosY = (float*)((DWORD)e2mfc + 0x61138);
+			f10042294 = (float*)((DWORD)e2mfc + 0x42294);
+			injector::MakeJMP((DWORD)e2mfc + 0xF2B3, P_HudPosHook, true);
+			jmpAddr3 = (DWORD)e2mfc + 0xF2B3 + 0x45;
+
+			if (bWidescreenHud)
 			{
-				Sleep(1);
-				if ((unsigned char)*(DWORD*)nComicsCheck == 0xAE)
+				Screen.fHudOffsetWide = fWidescreenHudOffset;
+
+				if (Screen.fAspectRatio < (16.0f / 9.0f))
 				{
-					stdViewPortSize = 480.0f * (fAspectRatio);
-					CPatch::SetFloat((DWORD)h_e2mfc_dll + 0x434F4, 480.0f); //480.0f
+					Screen.fHudOffsetWide = fWidescreenHudOffset / (((16.0f / 9.0f) / (Screen.fAspectRatio)) * 1.5f);
 				}
-				else
-				{
-					stdViewPortSize = 640.0f;
-					CPatch::SetFloat((DWORD)h_e2mfc_dll + 0x434F4, 480.0f); //480.0f
-				}
-			}
-			else
-			{
-				Sleep(1);
-				if ((unsigned char)*(DWORD*)nComicsCheck == 0xAE)
-				{
-					stdViewPortSize = (480.0f * (fAspectRatio) / 1.17936117936f);
-					CPatch::SetFloat((DWORD)h_e2mfc_dll + 0x434F4, 480.0f / 1.17936117936f); //480.0f
-				}
-				else
-				{
-					stdViewPortSize = 640.0f;
-					CPatch::SetFloat((DWORD)h_e2mfc_dll + 0x434F4, 480.0f); //480.0f
-				}
+
+				pTextElementPosX = (TextCoords*)((DWORD)e2mfc + 0x60374);
+				injector::MakeJMP((DWORD)e2mfc + 0xAACC, P_TextPosHook, true);
+				jmpAddr6 = (DWORD)e2mfc + 0xAACC + 0x6;
 			}
 		}
 
+		CreateThread(0, 0, (LPTHREAD_START_ROUTINE)&ComicsHandler, NULL, 0, NULL);
 	}
 	return 0;
 }
+
+
 
 BOOL APIENTRY DllMain(HMODULE /*hModule*/, DWORD reason, LPVOID /*lpReserved*/)
 {
