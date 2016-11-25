007432cff9c774e36b3d3e83c3e5757efdb3f7e6
ThirteenAG <thirteenag@gmail.com>
2016-11-25 14:53:42 +0300

    WFP v5.5.3
    - fixes #171
    - fixes #150

--- SecondSight.WidescreenFix/dllmain.cpp
+++ SecondSight.WidescreenFix/dllmain.cpp
@@ -61,11 +61,11 @@ DWORD WINAPI Init(LPVOID)
 
 	static float fARFactor = 0.25f * Screen.fAspectRatio;
 	pattern = hook::pattern("D8 0D ? ? ? ? 51 DB 05 ? ? ? ? 8B");
-	injector::WriteMemory(pattern.get(1).get<uint32_t>(2), &fARFactor); //0x43D26B
-
+	injector::WriteMemory(pattern.get(1).get<uint32_t>(2), &fARFactor, true); //0x43D26B
+	
 	pattern = hook::pattern("D8 0D ? ? ? ? 33 C9 D9 44 24 14 89 48 04 D8 64 24 10");
-	injector::WriteMemory(pattern.get(0).get<uint32_t>(2), &Screen.fFieldOfView); //0x5E4B08
-
+	injector::WriteMemory(pattern.get(0).get<uint32_t>(2), &Screen.fFieldOfView, true); //0x5E4B08
+	
 	pattern = hook::pattern("68 ? ? ? ? 52 E8 ? ? ? ? 8D 44 24 60 83");
 	injector::WriteMemory<float>(pattern.get(0).get<uint32_t>(1), Screen.fAspectRatio, true); //0x55BEB0
 	pattern = hook::pattern("D8 0D ? ? ? ? D9 9C 24 88 00 00 00 E8");
--- SonicHeroes.WidescreenFix/dllmain.cpp
+++ SonicHeroes.WidescreenFix/dllmain.cpp
@@ -16,7 +16,7 @@ struct Screen
 
 DWORD WINAPI Init(LPVOID)
 {
-	auto pattern = hook::pattern("BF 94 00 00 00 8B C7");
+	auto pattern = hook::pattern("0F BE 0D ? ? ? ? 8D 0C 89 8B");
 	if (!(pattern.size() > 0) && !bDelay)
 	{
 		bDelay = true;
@@ -27,7 +27,7 @@ DWORD WINAPI Init(LPVOID)
 	if (bDelay)
 	{
 		while (!(pattern.size() > 0))
-			pattern = hook::pattern("BF 94 00 00 00 8B C7");
+			pattern = hook::pattern("0F BE 0D ? ? ? ? 8D 0C 89 8B");
 	}
 
 	CIniReader iniReader("");
@@ -58,11 +58,68 @@ DWORD WINAPI Init(LPVOID)
 		}
 	}; injector::MakeInline<ResHook>(pattern.get(0).get<uint32_t>(0), pattern.get(0).get<uint32_t>(27));
 
-	//pattern = hook::pattern("68 AB AA AA 3F");
-	//for (size_t i = 0; i < pattern.size(); ++i)
-	//{
-	//	injector::WriteMemory<float>(pattern.get(i).get<uint32_t>(1), Screen.fAspectRatio, true);
-	//}
+	injector::WriteMemory<uint8_t>(0x446B06, 0xEB, true);
+
+	pattern = hook::pattern("A3 ? ? ? ? 8B 42 08 A3 ? ? ? ? 8B"); //655123
+	static auto pResY = *pattern.get(0).get<uint32_t*>(1);
+	struct ResHook2
+	{
+		void operator()(injector::reg_pack& regs)
+		{
+			*(pResY - 1) = Screen.Width;
+			*(pResY - 0) = Screen.Height;
+		}
+	}; injector::MakeInline<ResHook2>(pattern.get(0).get<uint32_t>(0));
+	
+	pattern = hook::pattern("A3 ? ? ? ? A1 ? ? ? ? 68 ? ? ? ? 50 8B 08"); //6557A8
+	struct ResHook2A
+	{
+		void operator()(injector::reg_pack& regs)
+		{
+			*(pResY - 1) = Screen.Width;
+			*(pResY - 0) = Screen.Height;
+		}
+	}; injector::MakeInline<ResHook2A>(pattern.get(0).get<uint32_t>(0));
+	
+	pattern = hook::pattern("89 0D ? ? ? ? 8B 6C 24 7C 8B 75 60 80 7E 20 05"); //657047
+	struct ResHook3
+	{
+		void operator()(injector::reg_pack& regs)
+		{
+			*(pResY - 1) = Screen.Width;
+			*(pResY - 0) = Screen.Height;
+		}
+	}; injector::MakeInline<ResHook3>(pattern.get(0).get<uint32_t>(0), pattern.get(0).get<uint32_t>(6));
+	
+	pattern = hook::pattern("89 15 ? ? ? ? 8B 51 08 89 15 ? ? ? ? 8B 49 0C"); //
+	struct ResHook4
+	{
+		void operator()(injector::reg_pack& regs)
+		{
+			*(pResY - 1) = Screen.Width;
+			*(pResY - 0) = Screen.Height;
+		}
+	}; injector::MakeInline<ResHook4>(pattern.get(2).get<uint32_t>(0), pattern.get(2).get<uint32_t>(6));
+	
+	pattern = hook::pattern("89 0D ? ? ? ? 5F 5E 5D B8 01 00 00 00"); //657CAF
+	struct ResHook5
+	{
+		void operator()(injector::reg_pack& regs)
+		{
+			*(pResY - 1) = Screen.Width;
+			*(pResY - 0) = Screen.Height;
+		}
+	}; injector::MakeInline<ResHook5>(pattern.get(0).get<uint32_t>(0), pattern.get(0).get<uint32_t>(6));
+
+	pattern = hook::pattern("8D B2 94 00 00 00 D9"); //0x64AFA5
+	struct CutOffAreaHook
+	{
+		void operator()(injector::reg_pack& regs)
+		{
+			regs.esi = regs.edx + 0x94;
+			*(float*)(regs.edx + 0x68) /= Screen.fHudScale;
+		}
+	}; injector::MakeInline<CutOffAreaHook>(pattern.get(0).get<uint32_t>(0), pattern.get(0).get<uint32_t>(6));
 	
 	pattern = hook::pattern("D9 05 ? ? ? ? 89 4E 68 8B 50 04 D8 76 68"); //0x64AC8B
 	injector::WriteMemory(pattern.get(0).get<uint32_t>(2), &Screen.fHudScale, true);
