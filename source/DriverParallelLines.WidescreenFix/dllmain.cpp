1eaf5ad3feb6ed667dc8952693d5766260d9d625
ThirteenAG <thirteenag@gmail.com>
2017-03-01 13:44:46 +0300

    submodules update
    -dpl 2D fix, radar is still broken.

--- source/DriverParallelLines.WidescreenFix/dllmain.cpp
+++ source/DriverParallelLines.WidescreenFix/dllmain.cpp
@@ -25,6 +25,90 @@ struct Screen
     float fWidth43;
 } Screen;
 
+enum ScreenModes
+{
+    LETTERBOX = 0,
+    UNKNOWN = 1,
+    WIDE16BY9 = 2
+};
+
+void __fastcall sub_5F3627(uintptr_t _this, uint32_t edx, uintptr_t a2, uintptr_t a3)
+{
+    //DWORD* unk_933E1C = (DWORD*)0x933E1C;
+    //uintptr_t v3 = unk_933E1C[(*(uintptr_t*)(_this + 4)) + 257];
+    size_t j = 0;
+    //float w = *(float *)(v3 + 0x3F4);
+   // float h = *(float *)(v3 + 0x3F8);
+    float v4 = 1.0f / Screen.fWidth;
+    float v5 = 1.0f / Screen.fHeight;
+    uintptr_t v6 = *(uintptr_t*)(a2 + 8);
+    if (*(uint32_t*)a2)
+    {
+        do
+        {
+            *(float*)(a3 + 00) = *(float*)(v6 + 16);
+            *(float*)(a3 + 04) = *(float*)(v6 + 20) + *(float *)(v6 + 28);
+            *(float*)(a3 + 16) = *(float*)(v6 + 32);
+            *(float*)(a3 + 20) = *(float*)(v6 + 44);
+            *(float*)(a3 + 24) = *(float*)(v6 + 16);
+            *(float*)(a3 + 28) = *(float*)(v6 + 20);
+            *(float*)(a3 + 40) = *(float*)(v6 + 32);
+            *(float*)(a3 + 44) = *(float*)(v6 + 36);
+            *(float*)(a3 + 48) = *(float*)(v6 + 24) + *(float *)(v6 + 16);
+            *(float*)(a3 + 52) = *(float*)(v6 + 20) + *(float *)(v6 + 28);
+            *(float*)(a3 + 64) = *(float*)(v6 + 40);
+            *(float*)(a3 + 68) = *(float*)(v6 + 44);
+            *(float*)(a3 + 72) = *(float*)(v6 + 24) + *(float *)(v6 + 16);
+            *(float*)(a3 + 76) = *(float*)(v6 + 20);
+            *(float*)(a3 + 88) = *(float*)(v6 + 40);
+            *(float*)(a3 + 92) = *(float*)(v6 + 36);
+
+            uintptr_t v8 = a3 + 4;
+
+            struct flt
+            {
+                float f1;
+                float f2;
+            };
+
+            flt floats[4];
+
+            for (size_t i = 0; i < 4; i++)
+            {
+                floats[i].f1 = *(float*)(v8 - 4);
+                floats[i].f2 = *(float*)(v8 - 0);
+                *(float*)(v8 - 4) = (((*(float*)(v8 - 4) * 2.0f) - 1.0f) - v4);
+                *(float*)(v8 + 0) = ((1.0f - (*(float*)v8 * 2.0f)) - (0.0f - v5));
+                uint32_t v9 = (uint32_t)(*(float*)(v6 + 8) * 255.0f);
+                uint32_t v10 = (uint32_t)(*(float*)(v6 + 4) * 255.0f) & 0xFF | ((((uint32_t)(*(float*)(v6 + 12) * 255.0f) << 8) | (uint32_t)(*(float*)v6 * 255.0) & 0xFF) << 8);
+                *(uint32_t*)(v8 + 8) = (uint8_t)v9 | (v10 << 8);
+                *(uint32_t*)(v8 + 4) = 0;
+                v8 += 24;
+            }
+
+            uintptr_t v8a = a3 + 4;
+            for (size_t i = 0; i < 4; i++)
+            {
+                if ((floats[0].f1 == 0.0f && floats[0].f2 == 1.0f) && (floats[1].f1 == 0.0f && floats[1].f2 == 0.0f) &&
+                    (floats[2].f1 == 1.0f && floats[2].f2 == 1.0f) && (floats[3].f1 == 1.0f && floats[3].f2 == 0.0f))
+                {
+                    //to-do: fmv fix / radar
+                    *(float *)(v8a - 4) -= 0.5f;
+                }
+                else
+                {
+                    *(float *)(v8a - 4) /= (Screen.fAspectRatio / (16.0f / 9.0f));
+                }
+                v8a += 24;
+            }
+
+            ++j;
+            a3 += 96;
+            v6 += 48;
+        } while (j < *(uint32_t*)a2);
+    }
+}
+
 DWORD WINAPI Init(LPVOID)
 {
     auto pattern = hook::pattern("55 8B EC 81 EC 80 00 00 00");
@@ -86,19 +170,31 @@ DWORD WINAPI Init(LPVOID)
     {
         void operator()(injector::reg_pack& regs)
         {
-            *(uint32_t*)(regs.ecx + 0x10) = 2; //*(uint32_t*)(regs.esp + 0x04);
+            *(uint32_t*)(regs.ecx + 0x10) = WIDE16BY9; //*(uint32_t*)(regs.esp + 0x04);
         }
     }; injector::MakeInline<MenuAspectRatioSwitchHook>(pattern.get(0).get<uintptr_t>(0), pattern.get(0).get<uintptr_t>(7));
 
-    //pattern = hook::pattern(""); //0x
-    //struct HudHook
+    injector::MakeCALL(0x5F75AD, sub_5F3627, true);
+
+    //4016B4 - radar blips
+    //4B84F1 - radardisc
+    //4B9EF1 - radar renders here
+
+    //pattern = hook::pattern(""); //0x4B8522
+    //struct RadarDiscHook
     //{
     //    void operator()(injector::reg_pack& regs)
     //    {
-    //        regs.edi = regs.ebx | (regs.edi << 8);
-    //        //*(float*)(regs.edx - 0x04) /= 2.0f;
+    //        //*(float*)(regs.ebp - 0x30) = 0.0f;
+    //        //*(float*)(regs.ebp - 0x2C) = 0.0f;
+    //        //
+    //        //*(float*)(regs.ebp - 0x28) = 1.1f;
+    //        //*(float*)(regs.ebp - 0x24) = 1.0f;
+    //
+    //        *(uintptr_t*)(regs.ebp - 0x14) = *(uintptr_t*)(regs.ebx + 0x2C);
     //    }
-    //}; injector::MakeInline<HudHook>(0x5F379F, 0x5F379F + 7/*pattern.get(0).get<uintptr_t>(0), pattern.get(0).get<uintptr_t>(7)*/);
+    //}; injector::MakeInline<RadarDiscHook>(0x4B8522/*pattern.get(0).get<uintptr_t>(0), pattern.get(0).get<uintptr_t>(7)*/);
+
 
     return 0;
 }
