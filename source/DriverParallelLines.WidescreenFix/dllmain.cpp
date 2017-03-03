a3386ff209cd30b20e32f874246a180c376dda11
ThirteenAG <thirteenag@gmail.com>
2017-03-03 13:48:31 +0300

    submodules update

--- source/DriverParallelLines.WidescreenFix/dllmain.cpp
+++ source/DriverParallelLines.WidescreenFix/dllmain.cpp
@@ -9,19 +9,20 @@
 #define SCREEN_FOV_VERTICAL (2.0f * RADIAN_TO_DEGREE(atan(tan(DEGREE_TO_RADIAN(SCREEN_FOV_HORIZONTAL * 0.5f)) / SCREEN_AR_NARROW)))
 
 HWND hWnd;
-bool bDelay;
 
 struct Screen
 {
-    int Width;
-    int Height;
+    int32_t DesktopResW;
+    int32_t DesktopResH;
+    int32_t Width;
+    int32_t Height;
     float fWidth;
     float fHeight;
     float fFieldOfView;
     float fDynamicScreenFieldOfViewScale;
     float fAspectRatio;
     float fHudOffset;
-    int Width43;
+    int32_t Width43;
     float fWidth43;
 } Screen;
 
@@ -38,7 +39,7 @@ void __fastcall sub_5F3627(uintptr_t _this, uint32_t edx, uintptr_t a2, uintptr_
     //uintptr_t v3 = unk_933E1C[(*(uintptr_t*)(_this + 4)) + 257];
     size_t j = 0;
     //float w = *(float *)(v3 + 0x3F4);
-   // float h = *(float *)(v3 + 0x3F8);
+    // float h = *(float *)(v3 + 0x3F8);
     float v4 = 1.0f / Screen.fWidth;
     float v5 = 1.0f / Screen.fHeight;
     uintptr_t v6 = *(uintptr_t*)(a2 + 8);
@@ -93,7 +94,7 @@ void __fastcall sub_5F3627(uintptr_t _this, uint32_t edx, uintptr_t a2, uintptr_
                     (floats[2].f1 == 1.0f && floats[2].f2 == 1.0f) && (floats[3].f1 == 1.0f && floats[3].f2 == 0.0f))
                 {
                     //to-do: fmv fix / radar
-                    *(float *)(v8a - 4) -= 0.5f;
+                    //*(float *)(v8a - 4) -= 0.5f;
                 }
                 else
                 {
@@ -109,48 +110,64 @@ void __fastcall sub_5F3627(uintptr_t _this, uint32_t edx, uintptr_t a2, uintptr_
     }
 }
 
-DWORD WINAPI Init(LPVOID)
+DWORD WINAPI Init(LPVOID bDelay)
 {
-    auto pattern = hook::pattern("55 8B EC 81 EC 80 00 00 00");
-    if (!(pattern.size() > 0) && !bDelay)
+    auto pattern = hook::pattern("55 8B EC 83 EC 60 53 56 57");
+
+    if (pattern.empty() && !bDelay)
     {
-        bDelay = true;
-        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)&Init, NULL, 0, NULL);
+        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)&Init, (LPVOID)true, 0, NULL);
         return 0;
     }
 
     if (bDelay)
-    {
-        while (!(pattern.size() > 0))
-            pattern = hook::pattern("55 8B EC 81 EC 80 00 00 00");
-    }
+        while (pattern.clear().count_hint(1).empty()) { Sleep(0); };
 
-    CIniReader iniReader("");
-    Screen.Width = iniReader.ReadInteger("MAIN", "ResX", -1);
-    Screen.Height = iniReader.ReadInteger("MAIN", "ResY", -1);
 
-    if (!Screen.Width || !Screen.Height) {
-        HMONITOR monitor = MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST);
-        MONITORINFO info;
-        info.cbSize = sizeof(MONITORINFO);
-        GetMonitorInfo(monitor, &info);
-        Screen.Width = info.rcMonitor.right - info.rcMonitor.left;
-        Screen.Height = info.rcMonitor.bottom - info.rcMonitor.top;
-    }
+    HMONITOR monitor = MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST);
+    MONITORINFO info;
+    info.cbSize = sizeof(MONITORINFO);
+    GetMonitorInfo(monitor, &info);
+    Screen.DesktopResW = info.rcMonitor.right - info.rcMonitor.left;
+    Screen.DesktopResH = info.rcMonitor.bottom - info.rcMonitor.top;
+ 
+    //uncapping resolutions
+    pattern = hook::pattern("68 ? ? ? ? 68 ? ? ? ? 68 ? ? ? ? 68 ? ? ? ? FF 75 F8 BF"); //4C5A89
+    injector::WriteMemory(pattern.get_first<int32_t*>(1 +  0), INT_MAX, true);
+    injector::WriteMemory(pattern.get_first<int32_t*>(1 +  5), INT_MAX, true);
+    injector::WriteMemory(pattern.get_first<int32_t*>(1 + 10), 0, true);
+    injector::WriteMemory(pattern.get_first<int32_t*>(1 + 15), 0, true);
 
-    static auto pAspectRatio = *hook::pattern("D9 05 ? ? ? ? 53 8B 5D 08 8B 03 57 51").get_first<float*>(2);
-    static auto pFOV = *hook::pattern("D9 05 ? ? ? ? 8B 03 51 8B CB D9 1C 24 ").get_first<float*>(2);
+    //default to desktop res
+    pattern = hook::pattern("C7 46 4C ? ? ? ? C7 46 50 ? ? ? ? C7 46 54 ? ? ? ? C7"); //4C5077
+    injector::WriteMemory(pattern.get_first<int32_t*>(3 + 0), Screen.DesktopResW, true);
+    injector::WriteMemory(pattern.get_first<int32_t*>(3 + 7), Screen.DesktopResH, true);
+    
+    static auto pGameHwnd = *hook::get_pattern<HWND*>("A1 ? ? ? ? 85 C0 8D 7E 14 89", 1); //0x933E18
+    static auto pAspectRatio = *hook::get_pattern<float*>("D9 05 ? ? ? ? 53 8B 5D 08 8B 03 57 51", 2); //64B238
+    static auto pFOV = *hook::get_pattern<float*>("D9 05 ? ? ? ? 8B 03 51 8B CB D9 1C 24", 2); //64B234
+    static auto dword_71D4B4 = *hook::get_pattern<uintptr_t*>("A1 ? ? ? ? 8B 08 6A 00 50 FF 91 ? ? ? ? 8D 4E 18", 1);
+    static auto pGameResWidth = dword_71D4B4 + 2;
+    static auto pGameResHeight = dword_71D4B4 + 3;
 
     pattern = hook::pattern("89 86 6C 01 00 00 8B 45 F4"); //0x5E4C55
-    struct SetResHook
+    struct SetWindowedResHook
     {
         void operator()(injector::reg_pack& regs)
         {
-            if (Screen.Width == -1 || Screen.Height == -1)
-            {
-                Screen.Width = *(uint32_t*)(regs.ebp - 0x10);
-                Screen.Height = *(uint32_t*)(regs.ebp - 0x0C);
-            }
+            *(uint32_t*)(regs.esi + 0x16C) = Screen.DesktopResW;
+            *(uint32_t*)(regs.esi + 0x170) = Screen.DesktopResH;
+            SetWindowPos(*pGameHwnd, NULL, 0, 0, Screen.DesktopResW, Screen.DesktopResH, SWP_NOACTIVATE | SWP_NOZORDER);
+        }
+    }; injector::MakeInline<SetWindowedResHook>(pattern.get_first(0), pattern.get_first(15));
+
+    pattern = hook::pattern("C6 01 01 33 C0 C2 0C 00"); //0x5E4A6A
+    struct GetResHook
+    {
+        void operator()(injector::reg_pack& regs)
+        {
+            Screen.Width = *pGameResWidth;
+            Screen.Height = *pGameResHeight;
             Screen.fWidth = static_cast<float>(Screen.Width);
             Screen.fHeight = static_cast<float>(Screen.Height);
             Screen.fAspectRatio = (Screen.fWidth / Screen.fHeight);
@@ -158,12 +175,19 @@ DWORD WINAPI Init(LPVOID)
             Screen.fWidth43 = static_cast<float>(Screen.Width43);
             Screen.fDynamicScreenFieldOfViewScale = 2.0f * RADIAN_TO_DEGREE(atan(tan(DEGREE_TO_RADIAN(SCREEN_FOV_VERTICAL * 0.5f)) * (Screen.fWidth / Screen.fHeight))) * (1.0f / SCREEN_FOV_HORIZONTAL);
 
-            *(uint32_t*)(regs.esi + 0x16C) = Screen.Width;
-            *(uint32_t*)(regs.esi + 0x170) = Screen.Height;
             *pAspectRatio = Screen.fAspectRatio;
             *pFOV = Screen.fDynamicScreenFieldOfViewScale * 1.308f;
+
+            static tagRECT REKT;
+            REKT.left = (LONG)(((float)Screen.DesktopResW / 2.0f) - (Screen.fWidth / 2.0f));
+            REKT.top = (LONG)(((float)Screen.DesktopResH / 2.0f) - (Screen.fHeight / 2.0f));
+            REKT.right = (LONG)Screen.Width;
+            REKT.bottom = (LONG)Screen.Height;
+            SetWindowPos(*pGameHwnd, NULL, REKT.left, REKT.top, REKT.right, REKT.bottom, SWP_NOACTIVATE | SWP_NOZORDER);
+            *(uint8_t*)regs.ecx = 1;
+            regs.eax = 0;
         }
-    }; injector::MakeInline<SetResHook>(pattern.get(0).get<uintptr_t>(0), pattern.get(0).get<uintptr_t>(15));
+    }; injector::MakeInline<GetResHook>(pattern.get_first(0));
 
     pattern = hook::pattern("8B 44 24 04 89 41 10 B0 01 C2 04 00"); //0x57B954
     struct MenuAspectRatioSwitchHook
@@ -172,29 +196,31 @@ DWORD WINAPI Init(LPVOID)
         {
             *(uint32_t*)(regs.ecx + 0x10) = WIDE16BY9; //*(uint32_t*)(regs.esp + 0x04);
         }
-    }; injector::MakeInline<MenuAspectRatioSwitchHook>(pattern.get(0).get<uintptr_t>(0), pattern.get(0).get<uintptr_t>(7));
+    }; injector::MakeInline<MenuAspectRatioSwitchHook>(pattern.get_first(0), pattern.get_first(7));
 
-    injector::MakeCALL(0x5F75AD, sub_5F3627, true);
+    //2D
+    pattern = hook::pattern("E8 ? ? ? ? 8B 45 08 8B 4D 0C 89 45 E8 8B"); //0x5F75AD
+    injector::MakeCALL(pattern.get_first(0), sub_5F3627, true);
 
-    //4016B4 - radar blips
-    //4B84F1 - radardisc
-    //4B9EF1 - radar renders here
+    //Radar
+    pattern = hook::pattern("F3 0F 11 86 44 07 00 00 D9 40 14 51"); //0x4B874A
+    struct RadarPosHook
+    {
+        void operator()(injector::reg_pack& regs)
+        {
+            *(float*)(regs.esi + 0x744) = ((*(float *)(regs.eax + 0x10) + (1.0f / 640.0f / 2.0f)) / (Screen.fAspectRatio / (16.0f / 9.0f)));
+        }
+    }; injector::MakeInline<RadarPosHook>(pattern.get_first(0), pattern.get_first(8));
 
-    //pattern = hook::pattern(""); //0x4B8522
-    //struct RadarDiscHook
-    //{
-    //    void operator()(injector::reg_pack& regs)
-    //    {
-    //        //*(float*)(regs.ebp - 0x30) = 0.0f;
-    //        //*(float*)(regs.ebp - 0x2C) = 0.0f;
-    //        //
-    //        //*(float*)(regs.ebp - 0x28) = 1.1f;
-    //        //*(float*)(regs.ebp - 0x24) = 1.0f;
-    //
-    //        *(uintptr_t*)(regs.ebp - 0x14) = *(uintptr_t*)(regs.ebx + 0x2C);
-    //    }
-    //}; injector::MakeInline<RadarDiscHook>(0x4B8522/*pattern.get(0).get<uintptr_t>(0), pattern.get(0).get<uintptr_t>(7)*/);
 
+    pattern = hook::pattern("F3 0F 11 86 4C 07 00 00 D9 40 1C D9 1C 24 "); //0x4B877E
+    struct RadarScaleHook
+    {
+        void operator()(injector::reg_pack& regs)
+        {
+            *(float*)(regs.esi + 0x74C) = ((*(float *)(regs.eax + 0x18) - (1.0f / 640.0f / 2.0f)) / (Screen.fAspectRatio / (16.0f / 9.0f)));
+        }
+    }; injector::MakeInline<RadarScaleHook>(pattern.get_first(0), pattern.get_first(8));
 
     return 0;
 }
