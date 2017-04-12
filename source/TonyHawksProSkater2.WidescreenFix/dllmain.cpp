4532a8cc9b53564a4d65438956ade3cccebfe443
ThirteenAG <thirteenag@gmail.com>
2017-04-12 13:21:21 +0300

    thps2 proper fix

--- source/TonyHawksProSkater2.WidescreenFix/dllmain.cpp
+++ source/TonyHawksProSkater2.WidescreenFix/dllmain.cpp
@@ -1,87 +1,172 @@
 #include "stdafx.h"
-#include "..\includes\CPatch.h"
 
-HWND hWnd;
-
-int res_x, res_x43, res_y43, res_y;
-int res_x_default = 640;
-int res_y_default = 480;
-float hud_multiplier_x, hud_multiplier_y;
-
-void Init()
+struct Screen
 {
-	CIniReader iniReader("");
-	res_x = iniReader.ReadInteger("MAIN", "X", 0);
-	res_y = iniReader.ReadInteger("MAIN", "Y", 0);
+    int32_t Width;
+    int32_t Height;
+    float fWidth;
+    float fHeight;
+    int32_t Width43;
+    float fAspectRatio;
+    int32_t FOV;
+    float fHudOffset;
+    float fHudOffsetReal;
+} Screen;
 
-	if (!res_x || !res_y) {
-		HMONITOR monitor = MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST);
-		MONITORINFO info;
-		info.cbSize = sizeof(MONITORINFO);
-		GetMonitorInfo(monitor, &info);
-		res_x = info.rcMonitor.right - info.rcMonitor.left;
-		res_y = info.rcMonitor.bottom - info.rcMonitor.top;
-	}
+DWORD WINAPI Init(LPVOID bDelay)
+{
+    auto pattern = hook::pattern("53 56 57 89 65 E8 33 F6 39");
 
-	if (*(DWORD*)0x4F8436 == 640)
-	{
-		CPatch::SetUInt(0x4F8435 + 0x1, res_x);
-		CPatch::SetUInt(0x4F843A + 0x6, res_y);
-	}
-	else 
-	{
-		CPatch::Nop(0x4F3F27, 2);
-		CPatch::Nop(0x4F3F3B, 14);
+    if (pattern.empty() && !bDelay)
+    {
+        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)&Init, (LPVOID)true, 0, NULL);
+        return 0;
+    }
 
-		res_x43 = static_cast<int>(res_x / (4.0f / 3.0f));
-		res_y43 = static_cast<int>(res_y * (4.0f / 3.0f));
+    if (bDelay)
+        while (pattern.clear().count_hint(1).empty()) { Sleep(0); };
 
-		CPatch::SetUInt(0x4F5495 + 0x1, res_x);
-		CPatch::SetUInt(0x4F549A + 0x6, res_y43);
+    CIniReader iniReader("");
+    Screen.Width = iniReader.ReadInteger("MAIN", "ResX", 0);
+    Screen.Height = iniReader.ReadInteger("MAIN", "ResY", 0);
+    bool bFixHUD = iniReader.ReadInteger("MAIN", "FixHUD", 1) != 0;
 
-		CPatch::SetUInt(0x4F3F2F + 0x1, res_x);
-		CPatch::SetUInt(0x4F3F36 + 0x1, res_y43);
+    if (!Screen.Width || !Screen.Height)
+        std::tie(Screen.Width, Screen.Height) = GetDesktopRes();
 
-		hud_multiplier_x = 1.0f / res_x * (res_y / 480.0f);
-		hud_multiplier_y = 1.0f / res_x * (res_y43 / 480.0f);
+    Screen.fWidth = static_cast<float>(Screen.Width);
+    Screen.fHeight = static_cast<float>(Screen.Height);
+    Screen.Width43 = static_cast<int32_t>(Screen.fHeight * (4.0f / 3.0f));
+    Screen.fAspectRatio = (Screen.fWidth / Screen.fHeight);
+    Screen.fHudOffset = ((480.0f * Screen.fAspectRatio) - 640.0f) / 2.0f;
+    Screen.fHudOffsetReal = (Screen.fWidth - Screen.fHeight * (4.0f / 3.0f)) / 2.0f;
+    
+    pattern = hook::pattern("A3 ? ? ? ? E8 ? ? ? ? 8B 15 ? ? ? ? A1 ? ? ? ? 6A 10"); //4F3FEE
+    static int32_t* dword_29D6FE8 = *pattern.get_first<int32_t*>(1);
+    static int32_t* dword_29D6FE4 = *pattern.get_first<int32_t*>(17);
 
-			CPatch::SetFloat(0x516958, hud_multiplier_x);
-			CPatch::SetFloat(0x516954, hud_multiplier_y);
+    struct SetResHook1
+    {
+        void operator()(injector::reg_pack& regs)
+        {
+            *dword_29D6FE4 = Screen.Width;
+            *dword_29D6FE8 = Screen.Height;
+        }
+    }; injector::MakeInline<SetResHook1>(pattern.get_first(0));
 
-			CPatch::SetPointer(0x457244, &res_x43);
-			CPatch::SetPointer(0x457270, &res_x43);
-			CPatch::SetPointer(0x457293, &res_x43);
-			CPatch::SetPointer(0x4572BE, &res_x43);
+    pattern = hook::pattern("A3 ? ? ? ? 8B 0D ? ? ? ? 6A 10 51 50"); //4F54A4
+    struct SetResHook2
+    {
+        void operator()(injector::reg_pack& regs)
+        {
+            *dword_29D6FE4 = Screen.Width;
+            *dword_29D6FE8 = Screen.Height;
+        }
+    }; injector::MakeInline<SetResHook2>(pattern.get_first(0));
 
-			CPatch::SetPointer(0x457254 + 0x3, &res_y);
-			CPatch::SetPointer(0x45727B + 0x3, &res_y);
-			CPatch::SetPointer(0x4572A2 + 0x3, &res_y);
-			CPatch::SetPointer(0x4572CD + 0x3, &res_y);
+    pattern = hook::pattern("89 0D ? ? ? ? 8D 34 49"); //46463F
+    static int32_t* dword_55ED00 = *pattern.get_first<int32_t*>(2);
+    pattern = hook::pattern("A3 ? ? ? ? ? ? 8D 04 49 33 D2");
+    static int32_t* dword_55ED18 = *pattern.get_first<int32_t*>(1);
+    pattern = hook::pattern("C7 05 ? ? ? ? 00 10 00 00 5E A3"); 
+    static int32_t* dword_5606CC = *pattern.get_first<int32_t*>(2);
+    static int32_t* dword_5606D0 = *pattern.get_first<int32_t*>(12);
+    struct AspectRatioHook
+    {
+        void operator()(injector::reg_pack& regs)
+        {
+            *dword_55ED00 = Screen.Width;
+            *dword_55ED18 = Screen.Height;
+            *dword_5606D0 = 4096;
+            *dword_5606CC = 4096;
+        }
+    }; 
+    pattern = hook::pattern("E8 ? ? ? ? 83 C4 08 E8 ? ? ? ? 5E 5B 83 C4 08 C3"); //0x430466
+    injector::MakeInline<AspectRatioHook>(pattern.get_first(0));
+    pattern = hook::pattern("E8 ? ? ? ? 83 C4 08 53 56"); //0x4645B8
+    injector::MakeInline<AspectRatioHook>(pattern.get_first(0));
 
-			//main menu wheel fix
-			CPatch::SetPointer(0x4578B0 + 0x2BC + 0x3, &res_x_default);
-			CPatch::SetPointer(0x4578B0 + 0x2E9 + 0x3, &res_x_default);
-			CPatch::SetPointer(0x4578B0 + 0x314 + 0x3, &res_x_default);
-			CPatch::SetPointer(0x4578B0 + 0x33F + 0x3, &res_x_default);
+    //FOV
+    Screen.FOV = static_cast<int32_t>(4096.0f / (Screen.fAspectRatio / (4.0f / 3.0f)));
+    pattern = hook::pattern("F7 3D ? ? ? ? 33 C9 66 8B 4E"); //45E9E9
+    injector::WriteMemory(pattern.get_first(2), &Screen.FOV, true);
 
-			CPatch::SetPointer(0x4578B0 + 0x2D4 + 0x3, &res_y_default);
-			CPatch::SetPointer(0x4578B0 + 0x2FC + 0x3, &res_y_default);
-			CPatch::SetPointer(0x4578B0 + 0x327 + 0x3, &res_y_default);
-			CPatch::SetPointer(0x4578B0 + 0x352 + 0x3, &res_y_default);
+    //Main menu wheel fix
+    auto sub_4F0A70 = [](int16_t a1) -> int32_t
+    {
+        return (int32_t)(cos((float)(a1 & 0xFFF) * 0.00024414062f * 6.2831855f) * (4096.0f / (Screen.fAspectRatio / (4.0f / 3.0f))));
+    };
+    pattern = hook::pattern("E8 ? ? ? ? 53 89 44 24 1C 89");
+    injector::MakeCALL(pattern.get_first(0), static_cast<int32_t(*)(int16_t)>(sub_4F0A70), true); //0x457939
+    pattern = hook::pattern("E8 ? ? ? ? 0F AF 44 24 20 C1");
+    injector::MakeCALL(pattern.get_first(0), static_cast<int32_t(*)(int16_t)>(sub_4F0A70), true); //0x457992
 
-			CPatch::SetUChar(0x4F4003 + 0x1, 32);
+    auto sub_4F0AA0 = [](int16_t a1) -> int32_t
+    {
+        return (int32_t)(sin((float)(a1 & 0xFFF) * 0.00024414062f * 6.2831855f) * (4096.0f / (Screen.fAspectRatio / (4.0f / 3.0f))));
+    };
+    pattern = hook::pattern("E8 ? ? ? ? 8B F0 53 0F AF");
+    injector::MakeCALL(pattern.get_first(0), static_cast<int32_t(*)(int16_t)>(sub_4F0AA0), true); //0x457947
+    pattern = hook::pattern("E8 ? ? ? ? 8B 5D 0C 89 44");
+    injector::MakeCALL(pattern.get_first(0), static_cast<int32_t(*)(int16_t)>(sub_4F0AA0), true); //0x45795D
 
-			//CPatch::SetUInt(0x4F3F2F + 0x1, res_x43); //crop screen
-			CPatch::SetUInt(0x4F3F36 + 0x1, res_y43); //crop screen
-	}
+    if (bFixHUD)
+    {
+        //menu backgrounds
+        pattern = hook::pattern("A1 ? ? ? ? 81 EC 98"); //4D4BA0
+        auto sub_4D4BA0 = pattern.get_first(0);
+        static auto dword_29D6FF0 = *pattern.get_first<uint32_t*>(1);
+        struct MenuHook1
+        {
+            void operator()(injector::reg_pack& regs)
+            {
+                regs.eax = *dword_29D6FF0;
+                *(float*)(regs.esp + 0x8) += Screen.fHudOffset;
+            }
+        }; injector::MakeInline<MenuHook1>(sub_4D4BA0);
 
+        auto rpattern = hook::range_pattern((uintptr_t)sub_4D4BA0, (uintptr_t)sub_4D4BA0 + 0x414, pattern_str(0xA1, to_bytes(dword_29D6FE4))); //mov     eax, dword_29D6FE4
+        for (size_t i = 0; i < rpattern.size(); ++i)
+        {
+            //0x4D4CEF, 0x4D4D51, 0x4D4D8A, 0x4D4DBD, 0x4D4E4F, 0x4D4E82, 0x4D4EBB
+            injector::WriteMemory(rpattern.get(i).get<uint32_t>(1), &Screen.Width43, true);
+        }
+
+        //some menu things
+        pattern = hook::pattern("C7 41 28 00 00 80 BF"); //457C0E
+        struct MenuHook2
+        {
+            void operator()(injector::reg_pack& regs)
+            {
+                *(int16_t*)(regs.ecx + 0x08) += (int16_t)Screen.fHudOffsetReal;
+                *(int16_t*)(regs.ecx + 0x10) += (int16_t)Screen.fHudOffsetReal;
+                *(int16_t*)(regs.ecx + 0x18) += (int16_t)Screen.fHudOffsetReal;
+                *(int16_t*)(regs.ecx + 0x20) += (int16_t)Screen.fHudOffsetReal;
+                *(float*)(regs.ecx + 0x28) = -1.0f;
+            }
+        }; 
+
+        for (size_t i = 0; i < pattern.size(); ++i)
+        {
+            injector::MakeInline<MenuHook2>(pattern.get(i).get<uint32_t>(0), pattern.get(i).get<uint32_t>(7));
+        
+            rpattern = hook::range_pattern((uintptr_t)pattern.get(i).get<uint32_t>(-0x170), (uintptr_t)pattern.get(i).get<uint32_t>(0), pattern_str(0x0F, 0xAF, '?', to_bytes(dword_29D6FE4))); //imul    e?x, dword_29D6FE4
+            for (size_t i = 0; i < rpattern.size(); ++i)
+            {
+                injector::WriteMemory(rpattern.get(i).get<uint32_t>(3), &Screen.Width43, true);
+            }
+        }
+    }
+
+    return 0;
 }
 
 BOOL APIENTRY DllMain(HMODULE /*hModule*/, DWORD reason, LPVOID /*lpReserved*/)
 {
-	if (reason == DLL_PROCESS_ATTACH)
-	{
-		Init();
-	}
-	return TRUE;
+    if (reason == DLL_PROCESS_ATTACH)
+    {
+        //MessageBox(0,0,0,0);
+        Init(NULL);
+    }
+    return TRUE;
 }
