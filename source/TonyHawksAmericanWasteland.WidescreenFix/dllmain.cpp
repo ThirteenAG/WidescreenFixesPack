0c508a822b57c5803dabf060adce92377731cc96
ThirteenAG <thirteenag@gmail.com>
2017-04-18 13:50:39 +0300

    thaw - proper fix

--- source/TonyHawksAmericanWasteland.WidescreenFix/dllmain.cpp
+++ source/TonyHawksAmericanWasteland.WidescreenFix/dllmain.cpp
@@ -1,132 +1,148 @@
 #include "stdafx.h"
-#include "..\includes\CPatch.h"
+#include <random>
 
-#define _USE_MATH_DEFINES
-#include "math.h"
-HWND hWnd;
-
-int hud_patch;
-int res_x;
-int res_y;
-float fres_x, fres_y;
-float fDynamicScreenFieldOfViewScale;
-float fAspectRatio;
-float hud_multiplier_x;
-
-#define DEGREE_TO_RADIAN(fAngle) \
-    ((fAngle)* (float)M_PI / 180.0f)
-#define RADIAN_TO_DEGREE(fAngle) \
-    ((fAngle)* 180.0f / (float)M_PI)
-#define SCREEN_FOV_HORIZONTAL		124.59155f
-#define SCREEN_FOV_VERTICAL			(2.0f * RADIAN_TO_DEGREE(atan(tan(DEGREE_TO_RADIAN(SCREEN_FOV_HORIZONTAL * 0.5f)) / (4.0f / 3.0f))))
-
-void Init()
+struct Screen
 {
+    int32_t Width;
+    int32_t Height;
+    float fWidth;
+    float fHeight;
+    int32_t Width43;
+    float fAspectRatio;
+    float fAspectRatioDiff;
+    float fFieldOfView;
+    float fHUDScaleX;
+    float fHudOffset;
+    float fHudOffsetReal;
+} Screen;
+
+DWORD WINAPI Init(LPVOID bDelay)
+{
+    auto pattern = hook::pattern("89 0D ? ? ? ? 89 0D ? ? ? ? 8A 0D ? ? ? ? 84 C9");
+
+    if (pattern.count_hint(1).empty() && !bDelay)
+    {
+        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)&Init, (LPVOID)true, 0, NULL);
+        return 0;
+    }
+
+    if (bDelay)
+        while (pattern.clear().count_hint(1).empty()) { Sleep(0); };
+
     CIniReader iniReader("");
-    res_x = iniReader.ReadInteger("MAIN", "X", 0);
-    res_y = iniReader.ReadInteger("MAIN", "Y", 0);
-    hud_patch = iniReader.ReadInteger("MAIN", "HUD_PATCH", 0);
+    Screen.Width = iniReader.ReadInteger("MAIN", "ResX", 0);
+    Screen.Height = iniReader.ReadInteger("MAIN", "ResY", 0);
+    bool bFixHUD = iniReader.ReadInteger("MAIN", "FixHUD", 1) != 0;
+    bool bFixFOV = iniReader.ReadInteger("MAIN", "FixFOV", 1) != 0;
+    bool bRandomSongOrderFix = iniReader.ReadInteger("MAIN", "RandomSongOrderFix", 1) != 0;
 
-    if (!res_x || !res_y) {
-        HMONITOR monitor = MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST);
-        MONITORINFO info;
-        info.cbSize = sizeof(MONITORINFO);
-        GetMonitorInfo(monitor, &info);
-        res_x = info.rcMonitor.right - info.rcMonitor.left;
-        res_y = info.rcMonitor.bottom - info.rcMonitor.top;
-    }
+    if (!Screen.Width || !Screen.Height)
+        std::tie(Screen.Width, Screen.Height) = GetDesktopRes();
 
-    if ((*(DWORD*)0x53511E == 1280))
+    Screen.fWidth = static_cast<float>(Screen.Width);
+    Screen.fHeight = static_cast<float>(Screen.Height);
+    Screen.Width43 = static_cast<int32_t>(Screen.fHeight * (4.0f / 3.0f));
+    Screen.fAspectRatio = (Screen.fWidth / Screen.fHeight);
+    Screen.fAspectRatioDiff = 1.0f / (Screen.fAspectRatio / (4.0f / 3.0f));
+    Screen.fHUDScaleX = 1.0f / Screen.fWidth * (Screen.fHeight / 480.0f);
+    Screen.fHudOffset = ((480.0f * Screen.fAspectRatio) - 640.0f) / 2.0f;
+    Screen.fHudOffsetReal = (Screen.fWidth - Screen.fHeight * (4.0f / 3.0f)) / 2.0f;
+
+    //Resolution
+    static int32_t* dword_6D2224 = *pattern.get_first<int32_t*>(2);
+    static int32_t* dword_6D2228 = *hook::get_pattern<int32_t*>("89 3D ? ? ? ? 89 3D ? ? ? ? 89 15 ? ? ? ? 89 15 ? ? ? ? 74", 2);
+    struct SetResHook
     {
-        CPatch::SetUInt(0x6D2224, res_x);
-        CPatch::SetUInt(0x6D2228, res_y);
-
-        fres_x = static_cast<float>(res_x);
-        fres_y = static_cast<float>(res_y);
-
-        CPatch::SetPointer(0x4370CB + 0x4, &fres_x);
-        CPatch::SetPointer(0x4370F7 + 0x4, &fres_y);
-
-        CPatch::SetPointer(0x4370CB + 0x4, &fres_x);
-        CPatch::SetPointer(0x4370F7 + 0x4, &fres_y);
-
-        CPatch::SetUInt(0x53510D + 0x1, res_x);
-        CPatch::SetUInt(0x53511A + 0x4, res_x);
-        CPatch::SetUInt(0x535122 + 0x4, res_x);
-        CPatch::SetUInt(0x53512A + 0x4, res_x);
-
-        CPatch::SetUInt(0x53514A + 0x4, res_y);
-        CPatch::SetUInt(0x535152 + 0x4, res_y);
-        CPatch::SetUInt(0x53515A + 0x4, res_y);
-
-        fAspectRatio = fres_x / fres_y;
-        fDynamicScreenFieldOfViewScale = 2.0f * RADIAN_TO_DEGREE(atan(tan(DEGREE_TO_RADIAN(SCREEN_FOV_VERTICAL * 0.5f)) * fAspectRatio));
-
-        CPatch::SetFloat(0x691FA8, fDynamicScreenFieldOfViewScale);
-        CPatch::SetPointer(0x4EDA60 + 0x2, &fAspectRatio);
-
-        hud_multiplier_x = 1.0f / res_x * (res_y / 480.0f);
-        if (hud_patch)
+        void operator()(injector::reg_pack& regs)
         {
-            //CPatch::SetFloat(0x694E1C, hud_multiplier_x);
-            //CPatch::SetPointer(0x50D20E + 0x4, &hud_multiplier_x);
-            CPatch::SetPointer(0x5352B1 + 0x2, &hud_multiplier_x);
+            *dword_6D2224 = Screen.Width;
+            *dword_6D2228 = Screen.Height;
         }
+    };
+    pattern = hook::pattern(pattern_str(0xA3, to_bytes(dword_6D2224)));
+    for (size_t i = 0; i < pattern.size(); ++i)
+        injector::MakeInline<SetResHook>(pattern.get(i).get<uint32_t>(0));
 
-        CPatch::Nop(0x535035, 6);
-        CPatch::Nop(0x5352D9, 10);
-        DWORD nHudAlignment = static_cast<DWORD>((fres_x - (fres_y * (4.0f / 3.0f))) / 2);
-        CPatch::SetUInt(0x7F3D5C, nHudAlignment);
-    }
-    else
+    pattern = hook::pattern(pattern_str(0x89, '?', to_bytes(dword_6D2224)));
+    if (!pattern.count_hint(1).empty())
+        injector::MakeInline<SetResHook>(pattern.get_first(0), pattern.get_first(6));
+
+    pattern = hook::pattern(pattern_str(0x89, '?', to_bytes(dword_6D2228)));
+    injector::MakeInline<SetResHook>(pattern.count(2).get(0).get<void*>(0), pattern.count(2).get(0).get<void*>(6));
+    injector::MakeInline<SetResHook>(pattern.count(2).get(1).get<void*>(0), pattern.count(2).get(1).get<void*>(6));
+
+
+    //Aspect Ratio
+    pattern = hook::pattern("68 ? ? ? ? E8 ? ? ? ? 6A 00 68 ? ? ? ? E8 ? ? ? ? D9");
+    injector::WriteMemory<float>(pattern.get_first(1), Screen.fAspectRatio, true);
+
+    //FOV
+    if (bFixFOV)
     {
-        CPatch::SetUInt(0x6D2224, res_x);
-        CPatch::SetUInt(0x6D2228, res_y);
-
-        fres_x = static_cast<float>(res_x);
-        fres_y = static_cast<float>(res_y);
-
-        CPatch::SetPointer(0x4370CB + 0x4, &fres_x);
-        CPatch::SetPointer(0x4370F7 + 0x4, &fres_y);
-
-        CPatch::SetPointer(0x4370CB + 0x4, &fres_x);
-        CPatch::SetPointer(0x4370F7 + 0x4, &fres_y);
-
-        CPatch::SetUInt(0x53510D + 0x1 + 0x240, res_x);
-        CPatch::SetUInt(0x53511A + 0x4 + 0x240, res_x);
-        CPatch::SetUInt(0x535122 + 0x4 + 0x240, res_x);
-        CPatch::SetUInt(0x53512A + 0x4 + 0x240, res_x);
-
-        CPatch::SetUInt(0x53514A + 0x4 + 0x240, res_y);
-        CPatch::SetUInt(0x535152 + 0x4 + 0x240, res_y);
-        CPatch::SetUInt(0x53515A + 0x4 + 0x240, res_y);
-
-        fAspectRatio = fres_x / fres_y;
-        fDynamicScreenFieldOfViewScale = 2.0f * RADIAN_TO_DEGREE(atan(tan(DEGREE_TO_RADIAN(SCREEN_FOV_VERTICAL * 0.5f)) * fAspectRatio));
-
-        CPatch::SetFloat(0x691FB0, fDynamicScreenFieldOfViewScale);
-        CPatch::SetPointer(0x4EDA80 + 0x2, &fAspectRatio);
-
-        hud_multiplier_x = 1.0f / res_x * (res_y / 480.0f);
-        if (hud_patch)
-        {
-            //CPatch::SetFloat(0x694E1C, hud_multiplier_x);
-            //CPatch::SetPointer(0x50D20E + 0x4, &hud_multiplier_x);
-            CPatch::SetPointer(0x5354F1 + 0x2, &hud_multiplier_x);
-
-            CPatch::Nop(0x535275, 6);
-            CPatch::Nop(0x535519, 10);
-            DWORD nHudAlignment = static_cast<DWORD>((fres_x - (fres_y * (4.0f / 3.0f))) / 2);
-            CPatch::SetUInt(0x7F3D5C, nHudAlignment);
-        }
+        pattern = hook::pattern("D8 0D ? ? ? ? D9 1C 24 E8 ? ? ? ? 8B 46 14");
+        #undef SCREEN_FOV_HORIZONTAL
+        #undef SCREEN_FOV_VERTICAL
+        #define SCREEN_FOV_HORIZONTAL 114.59155f
+        #define SCREEN_FOV_VERTICAL (2.0f * RADIAN_TO_DEGREE(atan(tan(DEGREE_TO_RADIAN(SCREEN_FOV_HORIZONTAL * 0.5f)) / SCREEN_AR_NARROW)))
+        float fDynamicScreenFieldOfViewScale = 2.0f * RADIAN_TO_DEGREE(atan(tan(DEGREE_TO_RADIAN(SCREEN_FOV_VERTICAL * 0.5f)) * Screen.fAspectRatio)) * (1.0f / SCREEN_FOV_HORIZONTAL);
+        injector::WriteMemory<float>(*pattern.get_first<float*>(2), SCREEN_FOV_HORIZONTAL * fDynamicScreenFieldOfViewScale, true);
     }
+
+    //HUD
+    if (bFixHUD)
+    {
+        static int32_t nHudOffset = static_cast<int32_t>(Screen.fHudOffsetReal);
+        static int32_t* dword_787D88;
+        struct SetOffsetHook
+        {
+            void operator()(injector::reg_pack& regs)
+            {
+                *dword_787D88 = nHudOffset;
+            }
+        };
+
+        pattern = hook::pattern("D8 0D ? ? ? ? 8B 35 ? ? ? ? 85 F6 D9 1D");
+        injector::WriteMemory(pattern.get_first(2), &Screen.fHUDScaleX, true);
+        dword_787D88 = *hook::get_pattern<int32_t*>("89 1D ? ? ? ? C7 05 ? ? ? ? 10 00 00 00", 2);
+
+        pattern = hook::pattern(pattern_str(0x89, '?', to_bytes(dword_787D88)));
+        injector::MakeInline<SetOffsetHook>(pattern.get_first(0), pattern.get_first(6));
+
+        pattern = hook::pattern(pattern_str(0xC7, '?', to_bytes(dword_787D88)));
+        injector::MakeInline<SetOffsetHook>(pattern.get_first(0), pattern.get_first(10));
+    }
+
+    if (bRandomSongOrderFix)
+    {
+        //pattern = hook::pattern("83 C4 08 ? D0 07 00 00");
+        //auto rpattern = hook::range_pattern((uintptr_t)pattern.get_first(3), (uintptr_t)pattern.get_first(110), "75 ? A1");
+        //static auto dword_69B718 = *rpattern.get_first<int32_t*>(3);
+        //static auto dword_681D14 = *hook::range_pattern((uintptr_t)pattern.get_first(3), (uintptr_t)pattern.get_first(60), "A1 ? ? ? ?").get_first<int32_t*>(1);
+        //struct RandomHook
+        //{
+        //    void operator()(injector::reg_pack& regs)
+        //    {
+        //        auto num_songs = *dword_681D14;
+        //        int32_t* sp_xbox_randomized_songs = dword_69B718;
+        //
+        //        std::vector<int32_t> songs;
+        //        songs.assign(std::addressof(sp_xbox_randomized_songs[0]), std::addressof(sp_xbox_randomized_songs[num_songs]));
+        //        std::mt19937 r{ std::random_device{}() };
+        //        std::shuffle(std::begin(songs), std::end(songs), r);
+        //
+        //        std::copy(songs.begin(), songs.end(), sp_xbox_randomized_songs);
+        //    }
+        //}; injector::MakeInline<RandomHook>(pattern.get_first(3), rpattern.get_first(2));
+    }
+
+    return 0;
 }
 
 BOOL APIENTRY DllMain(HMODULE /*hModule*/, DWORD reason, LPVOID /*lpReserved*/)
 {
     if (reason == DLL_PROCESS_ATTACH)
     {
-        Init();
+        Init(NULL);
     }
     return TRUE;
 }
