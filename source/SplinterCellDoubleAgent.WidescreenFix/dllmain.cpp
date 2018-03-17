4638f35160f966ad0c58d89fe636285fa37be21a
ThirteenAG <thirteenag@gmail.com>
2018-03-18 01:18:16 +0300

    scda - scope fix, fullscreen textures restored

--- source/SplinterCellDoubleAgent.WidescreenFix/dllmain.cpp
+++ source/SplinterCellDoubleAgent.WidescreenFix/dllmain.cpp
@@ -18,7 +18,7 @@ struct Screen
     float fDynamicScreenFieldOfViewScale;
     float fHudOffset;
     float fAspectRatio;
-    float fHUDScaleX;
+    float fHudScaleX;
     float fTextScaleX;
     int32_t FilmstripScaleX;
     int32_t FilmstripOffset;
@@ -57,6 +57,32 @@ DWORD WINAPI InitD3DDrv(LPVOID bDelay)
         }
     }; injector::MakeInline<RenderFilmstrip_Hook2>(pattern.get_first(0), pattern.get_first(6));
     injector::WriteMemory<uint16_t>(pattern.get_first(6), 0xD285, true);     //test    edx, edx
+
+    //ScopeLens
+    pattern = hook::module_pattern(GetModuleHandle("D3DDrv"), "F3 0F 2A 8F ? ? ? ? 8B AC 24");
+    struct ScopeLens_Hook1 // stretching scope image horizontally
+    {
+        void operator()(injector::reg_pack& regs)
+        {
+            auto t = Screen.fWidth * Screen.fAspectRatio;
+            _asm movss xmm1, dword ptr[t]
+        }
+    }; injector::MakeInline<ScopeLens_Hook1>(pattern.get_first(0), pattern.get_first(8));
+
+    pattern = hook::module_pattern(GetModuleHandle("D3DDrv"), "8B 80 ? ? ? ? 8B 08 6A 14 8D 54 24 70 52 6A 02 6A 05");
+    struct ScopeLens_Hook2 // and then fixing it along with overlay
+    {
+        void operator()(injector::reg_pack& regs)
+        {
+            regs.eax = *(uint32_t*)(regs.eax + 0x5F30);
+
+            auto pVertexStreamZeroData = regs.esp + 0x6C;
+            *(float*)(pVertexStreamZeroData + 0x00) /= Screen.fAspectRatio;
+            *(float*)(pVertexStreamZeroData + 0x14) /= Screen.fAspectRatio;
+            *(float*)(pVertexStreamZeroData + 0x28) /= Screen.fAspectRatio;
+            *(float*)(pVertexStreamZeroData + 0x3C) /= Screen.fAspectRatio;
+        }
+    }; injector::MakeInline<ScopeLens_Hook2>(pattern.get_first(0), pattern.get_first(6));
 
     //crashfix
     pattern = hook::module_pattern(GetModuleHandle("D3DDrv"), "8D B5 64 98 00 00 6A 04 8B CE F3 0F 11 44 24 18"); //
@@ -101,13 +127,13 @@ DWORD WINAPI InitD3DDrv(LPVOID bDelay)
     }; injector::MakeInline<FlushHook>(pattern.get_first(0), pattern.get_first(36));
 
     pattern = hook::module_pattern(GetModuleHandle("D3DDrv"), "8B 95 ? ? ? ? 8B 85 ? ? ? ? 8B 08 52 50 FF 91 ? ? ? ? 8B 8D"); //
-    struct test //vertex shader for alt tab
+    struct RenderFilmstripHook4 //vertex shader for alt tab
     {
         void operator()(injector::reg_pack& regs)
         {
             regs.edx = NULL;
         }
-    }; injector::MakeInline<test>(pattern.get_first(0), pattern.get_first(6));
+    }; injector::MakeInline<RenderFilmstripHook4>(pattern.get_first(0), pattern.get_first(6));
 
     pattern = hook::module_pattern(GetModuleHandle("D3DDrv"), "8B 7B 64 85 FF 8B CE 6A 04 74 1F 89 BD"); //alpha, settexture, maybe not needed
     injector::WriteMemory(pattern.get_first(0), 0x85909090, true);
@@ -141,9 +167,53 @@ DWORD WINAPI InitEngine(LPVOID bDelay)
     if (bDelay)
         while (pattern.clear(GetModuleHandle("Engine")).count_hint(1).empty()) { Sleep(0); };
 
-    auto pWidthScale = *pattern.get_first<float*>(4); //?m_widthScale@ImageUnreal@magma@@0MA
-    injector::MakeNOP(pattern.get_first(), 8, true); //?InitDraw@ImageUnreal@magma@@SAXXZ + 0x153
-    injector::WriteMemory<float>(pWidthScale, Screen.fHUDScaleX, true);
+    static auto GIsWideScreen = hook::module_pattern(GetModuleHandle("Engine"), "8B 0D ? ? ? ? F3 0F 59 D9 F3 0F 10 0D ? ? ? ? F3 0F 5C D8 F3 0F 58 DC").get_first<uint8_t>(2);
+    static auto GIsSameFrontBufferOnNormalTV = hook::module_pattern(GetModuleHandle("Engine"), "8B 15 ? ? ? ? 83 3A 00 74 58 F3 0F 10 88").get_first<uint8_t>(2);
+    pattern = hook::module_pattern(GetModuleHandle("Engine"), "83 39 00 75 63 8B 15 ? ? ? ? 83 3A 00"); //
+    static auto dword_109E09F0 = *pattern.get_first<float*>(60);
+    static auto dword_109E09F4 = dword_109E09F0 + 1;
+    static auto dword_109E09F8 = dword_109E09F0 + 2;
+    static auto dword_109E09FC = dword_109E09F0 + 3;
+    struct ImageUnrealDrawHook
+    {
+        void operator()(injector::reg_pack& regs)
+        {
+            //auto x = *(float*)&dword_109E09F0[regs.eax / 4];
+            //auto y = *(float*)&dword_109E09F4[regs.eax / 4];
+            auto w = *(float*)&dword_109E09F8[regs.eax / 4];
+            auto h = *(float*)&dword_109E09FC[regs.eax / 4];
+
+            if (!*GIsWideScreen && *GIsSameFrontBufferOnNormalTV)
+            {
+                *(float*)&dword_109E09F0[regs.eax / 4] *= 0.75f;
+                *(float*)&dword_109E09F4[regs.eax / 4] *= 0.75f;
+                *(float*)&dword_109E09F8[regs.eax / 4] *= 0.75f;
+                *(float*)&dword_109E09FC[regs.eax / 4] *= 0.75f;
+            }
+
+            if ((w == 1275.0f && h == 637.0f) || (w == 32.0f && h == 32.0f)) // fullscreen loadscreen and spinner
+            {
+                if (!(w == 32.0f && h == 32.0f))
+                {
+                    *(float*)&dword_109E09F4[regs.eax / 4] *= Screen.fHudScaleX;
+                    *(float*)&dword_109E09FC[regs.eax / 4] *= Screen.fHudScaleX;
+                }
+            }
+            else
+            {
+                if (w == 800.0f)
+                {
+                    *(float*)&dword_109E09F0[regs.eax / 4] *= Screen.fHudScaleX;
+                    *(float*)&dword_109E09F8[regs.eax / 4] *= Screen.fHudScaleX;
+                }
+                else
+                {
+                    *(float*)&dword_109E09F0[regs.eax / 4] /= Screen.fHudScaleX;
+                    *(float*)&dword_109E09F8[regs.eax / 4] /= Screen.fHudScaleX;
+                }
+            }
+        }
+    }; injector::MakeInline<ImageUnrealDrawHook>(pattern.get_first(0), pattern.get_first(104));
 
     pattern = hook::module_pattern(GetModuleHandle("Engine"), "F3 0F 5E CA F3 0F 11 4C 24 14 83 C4 04"); //0x1030678D
     struct TextHook
@@ -160,26 +230,8 @@ DWORD WINAPI InitEngine(LPVOID bDelay)
         }
     }; injector::MakeInline<TextHook>(pattern.get_first(-4), pattern.get_first(4));
 
-
     pattern = hook::module_pattern(GetModuleHandle("Engine"), "F3 0F 5C 1D ? ? ? ? 0F 28 FD F3 0F 59 FA"); //0x103069A5 + 0x4
     injector::WriteMemory(pattern.get_first(4), &Screen.fHudOffset, true);
-
-    pattern = hook::module_pattern(GetModuleHandle("Engine"), "F3 0F 5C D8 F3 0F 58 DC"); //0x1030503C
-    struct HUDHook
-    {
-        void operator()(injector::reg_pack& regs)
-        {
-            static const float f1_0 = 1.0f;
-            _asm
-            {
-                movss xmm0, Screen.fHudOffset
-                subss xmm3, xmm0
-                addss xmm3, xmm4
-                movss xmm0, f1_0
-            }
-        }
-    }; injector::MakeInline<HUDHook>(pattern.get_first(0), pattern.get_first(8));
-
 
     //FOV
 #undef SCREEN_FOV_HORIZONTAL
@@ -193,7 +245,8 @@ DWORD WINAPI InitEngine(LPVOID bDelay)
     {
         void operator()(injector::reg_pack& regs)
         {
-            *(float*)&regs.edx = *(float*)(regs.ecx + 0x628) * Screen.fDynamicScreenFieldOfViewScale;
+            auto f = *(float*)(regs.ecx + 0x628) * Screen.fDynamicScreenFieldOfViewScale;
+            *(float*)&regs.edx = f > 170.0f ? 170.0f : f;
         }
     }; injector::MakeInline<UGameEngine_Draw_Hook>(pattern.get_first(0), pattern.get_first(6));
 
@@ -236,7 +289,7 @@ DWORD WINAPI Init(LPVOID bDelay)
             Screen.fWidth = static_cast<float>(Screen.Width);
             Screen.fHeight = static_cast<float>(Screen.Height);
             Screen.fAspectRatio = (Screen.fWidth / Screen.fHeight);
-            Screen.fHUDScaleX = 2.0f / (600.0f * Screen.fAspectRatio);
+            Screen.fHudScaleX = 1.0f / (((4.0f / 3.0f)) / (Screen.fAspectRatio));
             Screen.fTextScaleX = 600.0f * Screen.fAspectRatio;
             Screen.fHudOffset = ((4.0f / 3.0f) / Screen.fAspectRatio);
             Screen.FilmstripScaleX = static_cast<int32_t>(Screen.fWidth / (1280.0f / (368.0 * ((4.0 / 3.0) / (Screen.fAspectRatio)))));
