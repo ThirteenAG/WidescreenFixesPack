e5b15612a3c0f1445f7d75f4beaf4770fbf9fb7e
ThirteenAG <thirteenag@gmail.com>
2021-12-01 22:44:24 +0300

    gh actions update

--- premake5.lua
+++ premake5.lua
@@ -74,6 +74,27 @@ workspace "WidescreenFixesPack"
       includedirs { "external/asmjit/src" }
    end
    
+   function writeghaction(tag, prj_name)       
+      file = io.open(".github/workflows/" .. tag .. ".yml", "w")
+      if (file) then
+str = [[
+name: %s
+
+on:
+  workflow_dispatch:
+
+jobs:
+  call-workflow-passing-data:
+    uses: ThirteenAG/WidescreenFixesPack/.github/workflows/all.yml@master
+    with:
+      tag_list: %s
+      project: /t:%s
+]]
+         file:write(string.format(str, tag, tag, prj_name:gsub("%.", "_")))
+         file:close()
+      end
+   end
+   
    filter "configurations:Debug*"
       defines "DEBUG"
       symbols "On"
@@ -95,26 +116,36 @@ project "Assembly64.TestAsi"
    setpaths("./data/Assembly64.TestApp/", "Assembly64.TestApp.exe", "")
 project "Bully.WidescreenFix"
    setpaths("Z:/WFP/Games/Bully Scholarship Edition/", "Bully.exe", "plugins/")
+   writeghaction("bully", "Bully.WidescreenFix")
 project "Burnout3.PCSX2.WidescreenFix"
    configurations { "ReleasePCSX2", "DebugPCSX2" }
       setpaths("Z:/WFP/Games/PCSX2/", "pcsx2.exe")
       files { "includes/pcsx2/pcsx2.h" }
+      writeghaction("burnout3", "Burnout3.PCSX2.WidescreenFix")
 project "CallOfCthulhu.WidescreenFix"
    setpaths("Z:/WFP/Games/Call of Cthulhu/", "Engine/CoCMainWin32.exe", "Engine/scripts/")
+   writeghaction("callofcthulhu", "CallOfCthulhu.WidescreenFix")
 project "ColdFear.WidescreenFix"
    setpaths("Z:/WFP/Games/ColdFear/", "ColdFear_retail.exe")
+   writeghaction("coldfear", "ColdFear.WidescreenFix")
 project "Condemned.WidescreenFix"
    setpaths("Z:/WFP/Games/Condemned Criminal Origins/", "Condemned.exe")
+   writeghaction("condemned", "Condemned.WidescreenFix")
 project "DeerAvenger4.WidescreenFix"
    setpaths("Z:/WFP/Games/Deer Avenger 4/", "DA4.exe")
+   writeghaction("da4", "DeerAvenger4.WidescreenFix")
 project "Driv3r.WidescreenFix"
    setpaths("Z:/WFP/Games/DRIV3R/", "driv3r.exe")
+   writeghaction("driv3r", "Driv3r.WidescreenFix")
 project "DriverParallelLines.WidescreenFix"
    setpaths("Z:/WFP/Games/Driver Parallel Lines/", "DriverParallelLines.exe")
+   writeghaction("driverpl", "DriverParallelLines.WidescreenFix")
 project "EnterTheMatrix.WidescreenFix"
    setpaths("Z:/WFP/Games/Enter the Matrix/", "Matrix.exe")
+   writeghaction("enterthematrix", "EnterTheMatrix.WidescreenFix")
 project "FarCry.WidescreenFix"
    setpaths("Z:/WFP/Games/Far Cry/", "Bin32/FarCry.exe", "Bin32/")
+   writeghaction("farcry", "FarCry.WidescreenFix")
 project "FarCry64.WidescreenFix"
    platforms { "Win64" }
    architecture "x64"
@@ -123,10 +154,13 @@ project "FarCry64.WidescreenFix"
    targetdir "data/FarCry.WidescreenFix/Bin64/"
 project "Flatout.WidescreenFix"
    setpaths("Z:/WFP/Games/Flatout/FlatOut/", "flatout.exe")
+   writeghaction("flatout", "Flatout.WidescreenFix")
 project "Flatout2.WidescreenFix"
    setpaths("Z:/WFP/Games/Flatout/FlatOut2/", "FlatOut2.exe")
+   writeghaction("flatout2", "Flatout2.WidescreenFix")
 project "FlatoutUltimateCarnage.WidescreenFix"
    setpaths("Z:/WFP/Games/Flatout/FlatOut - Ultimate Carnage/", "Fouc.exe")
+   writeghaction("flatoutuc", "FlatoutUltimateCarnage.WidescreenFix")
 project "GettingUp.WidescreenFix"
    setpaths("Z:/WFP/Games/Marc Ecko's Getting Up 2/", "_Bin/GettingUp.exe", "_Bin/")
    targetdir "data/GettingUp.WidescreenFix/_Bin/"
@@ -134,160 +168,221 @@ project "GettingUp.WidescreenFix"
    buildoptions { "/Zc:threadSafeInit-" }
 project "GTA1.WidescreenFix"
    setpaths("Z:/WFP/Games/Grand Theft Auto/Grand Theft Auto 1 London 1969 1961/", "WINO/Grand Theft Auto.exe", "WINO/scripts/")
+   writeghaction("gta1", "GTA1.WidescreenFix")
 project "GTA2.WidescreenFix"
    setpaths("Z:/WFP/Games/Grand Theft Auto/Grand Theft Auto 2/", "gta2.exe")
+   writeghaction("gta2", "GTA2.WidescreenFix")
 project "GTA3.WidescreenFix"
    files { "includes/GTA/*.h", "includes/GTA/*.cpp" }
    setpaths("Z:/WFP/Games/Grand Theft Auto/GTAIII/", "gta3.exe")
+   writeghaction("gta3", "GTA3.WidescreenFix")
 project "GTA3DE.FusionMod"
    platforms { "Win64" }
    architecture "x64"
    add_asmjit()
    setpaths("Z:/WFP/Games/Grand Theft Auto The Definitive Edition/GTA III - Definitive Edition/", "Gameface/Binaries/Win64/LibertyCity.exe", "Gameface/Binaries/Win64/scripts/")
+   writeghaction("gta3de", "GTA3DE.FusionMod")
 project "GTAVC.WidescreenFix"
    files { "includes/GTA/*.h", "includes/GTA/*.cpp" }
    setpaths("Z:/WFP/Games/Grand Theft Auto/Grand Theft Auto Vice City/", "gta-vc.exe")
+   writeghaction("gtavc", "GTAVC.WidescreenFix")
 project "GTAVCDE.FusionMod"
    platforms { "Win64" }
    architecture "x64"
    add_asmjit()
    setpaths("Z:/WFP/Games/Grand Theft Auto The Definitive Edition/GTA Vice City - Definitive Edition/", "Gameface/Binaries/Win64/ViceCity.exe", "Gameface/Binaries/Win64/scripts/")
+   writeghaction("gtavcde", "GTAVCDE.FusionMod")
 project "GTASA.WidescreenFix"
    files { "includes/GTA/*.h", "includes/GTA/*.cpp" }
    setpaths("Z:/WFP/Games/Grand Theft Auto/GTA San Andreas/", "gta_sa.exe")
+   writeghaction("gtasa", "GTASA.WidescreenFix")
 project "GTASADE.FusionMod"
    platforms { "Win64" }
    architecture "x64"
    add_asmjit()
    setpaths("Z:/WFP/Games/Grand Theft Auto The Definitive Edition/GTA San Andreas - Definitive Edition/", "Gameface/Binaries/Win64/SanAndreas.exe", "Gameface/Binaries/Win64/scripts/")
+   writeghaction("gtasade", "GTASADE.FusionMod")
 project "GTALCS.PCSX2.WidescreenFix"
    configurations { "ReleasePCSX2", "DebugPCSX2" }
       setpaths("Z:/WFP/Games/PCSX2/", "pcsx2.exe")
       files { "includes/pcsx2/pcsx2.h" }
+      writeghaction("gtalcs", "GTALCS.PCSX2.WidescreenFix")
 project "GTAVCS.PCSX2.WidescreenFix"
    configurations { "ReleasePCSX2", "DebugPCSX2" }
       setpaths("Z:/WFP/Games/PCSX2/", "pcsx2.exe")
       files { "includes/pcsx2/pcsx2.h" }
+      writeghaction("gtavcs", "GTAVCS.PCSX2.WidescreenFix")
 project "Gun.WidescreenFix"
    setpaths("Z:/WFP/Games/GUN/", "Gun.exe")
+   writeghaction("gun", "Gun.WidescreenFix")
 project "HiddenandDangerous2.WidescreenFix"
    setpaths("Z:/WFP/Games/Hidden and Dangerous 2/", "hd2.exe")
+   writeghaction("hd2", "HiddenandDangerous2.WidescreenFix")
 project "JustCause.WidescreenFix"
    setpaths("Z:/WFP/Games/Just Cause/", "JustCause.exe")
+   writeghaction("justcause", "JustCause.WidescreenFix")
 project "KingKong.WidescreenFix"
    setpaths("Z:/WFP/Games/King Kong/", "CheckApplication.exe")
+   writeghaction("kingkong", "KingKong.WidescreenFix")
 project "KnightRider.WidescreenFix"
    configurations { "Release", "Debug", "ReleasePCSX2", "DebugPCSX2" }
       setpaths("Z:/WFP/Games/Knight Rider/", "Knight Rider.exe")
       files { "includes/pcsx2/pcsx2.h" }
+	  writeghaction("kr", "KnightRider.WidescreenFix")
    filter "configurations:*PCSX2"
       setpaths("Z:/WFP/Games/PCSX2/", "pcsx2.exe")
 project "KnightRider2.WidescreenFix"
    configurations { "Release", "Debug", "ReleasePCSX2", "DebugPCSX2" }
       setpaths("Z:/WFP/Games/Knight Rider 2/", "KR2.exe")
       files { "includes/pcsx2/pcsx2.h" }
+	  writeghaction("kr2", "KnightRider2.WidescreenFix")
    filter "configurations:*PCSX2"
       setpaths("Z:/WFP/Games/PCSX2/", "pcsx2.exe")
 project "LARush.WidescreenFix"
    setpaths("Z:/WFP/Games/LA Rush/", "LARush.exe", "plugins/")
+   writeghaction("larush", "LARush.WidescreenFix")
 project "Mafia.GenericFix"
    setpaths("Z:/WFP/Games/Mafia/", "Setup.exe")
    targetdir "data/Mafia.WidescreenFix/scripts"
 project "Mafia.WidescreenFix"
    setpaths("Z:/WFP/Games/Mafia/", "GameV12.exe")
+   writeghaction("mafia", "Mafia.WidescreenFix")
 project "Mafia.CXBXR.WidescreenFix"
    configurations { "ReleaseCXBXR", "DebugCXBXR" }
       setpaths("Z:/WFP/Games/CXBXR/", "cxbx.exe")
       files { "includes/cxbxr/cxbxr.h" }
+   writeghaction("mafiaxbox", "Mafia.CXBXR.WidescreenFix")
 project "Manhunt.WidescreenFix"
    setpaths("Z:/WFP/Games/Manhunt/", "manhunt.exe")
+   writeghaction("manhunt", "Manhunt.WidescreenFix")
 project "MassEffectTrilogy.FOVFix"
    setpaths("Z:/WFP/Games/Mass Effect/Mass Effect 2/Binaries/", "MassEffect2.exe", "/")
+   writeghaction("masseffect", "MassEffectTrilogy.FOVFix")
 project "MaxPayne.WidescreenFix"
    setpaths("Z:/WFP/Games/Max Payne/Max Payne/", "MaxPayne.exe")
+   writeghaction("mp1", "MaxPayne.WidescreenFix")
 project "MaxPayne2.WidescreenFix"
    setpaths("Z:/WFP/Games/Max Payne/Max Payne 2 The Fall of Max Payne/", "MaxPayne2.exe")
+   writeghaction("mp2", "MaxPayne2.WidescreenFix")
 project "NFSCarbon.WidescreenFix"
    setpaths("Z:/WFP/Games/Need For Speed/Need for Speed Carbon/", "NFSC.exe")
+   writeghaction("nfsc", "NFSCarbon.WidescreenFix")
 project "NFSMostWanted.WidescreenFix"
    setpaths("Z:/WFP/Games/Need For Speed/Need for Speed Most Wanted/", "speed.exe")
+   writeghaction("nfsmw", "NFSMostWanted.WidescreenFix")
 project "NFSProStreet.GenericFix"
    setpaths("Z:/WFP/Games/Need For Speed/Need for Speed ProStreet/", "nfsps.exe")
+   writeghaction("nfsps", "NFSProStreet.GenericFix")
 project "NFSUndercover.GenericFix"
    setpaths("Z:/WFP/Games/Need For Speed/Need for Speed Undercover/", "nfs.exe")
+   writeghaction("nfsuc", "NFSUndercover.GenericFix")
 project "NFSUnderground.WidescreenFix"
    setpaths("Z:/WFP/Games/Need For Speed/Need For Speed Underground/", "speed.exe")
    files { "textures/NFS/NFSU/icon.rc" }
    defines { "IDR_NFSUICON=200" }
+   writeghaction("nfsu", "NFSUnderground.WidescreenFix")
 project "NFSUnderground2.WidescreenFix"
    setpaths("Z:/WFP/Games/Need For Speed/Need For Speed Underground 2/", "speed2.exe")
+   writeghaction("nfsu2", "NFSUnderground2.WidescreenFix")
 project "Onimusha3.WidescreenFix"
    setpaths("Z:/WFP/Games/Onimusha 3/", "ONI3.exe")
+   writeghaction("onimusha3", "Onimusha3.WidescreenFix")
 project "ParadiseCracked.WidescreenFix"
    setpaths("Z:/WFP/Games/Paradise Cracked/", "game.exe")
+   writeghaction("paradisecracked", "ParadiseCracked.WidescreenFix")
 project "PsiOpsTheMindgateConspiracy.WidescreenFix"
    setpaths("Z:/WFP/Games/PSI-OPS/", "PsiOps.exe")
+   writeghaction("psiops", "PsiOpsTheMindgateConspiracy.WidescreenFix")
 project "Psychonauts.WidescreenFix"
    setpaths("Z:/WFP/Games/Psychonauts/", "Psychonauts.exe")
+   writeghaction("psychonauts", "Psychonauts.WidescreenFix")
 project "SecondSight.WidescreenFix"
    setpaths("Z:/WFP/Games/Second Sight/", "secondsight.exe")
+   writeghaction("secondsight", "SecondSight.WidescreenFix")
 project "SilentHill2.WidescreenFix"
    setpaths("Z:/WFP/Games/Silent Hill/Silent Hill 2/", "sh2pc.exe")
+   writeghaction("sh2", "SilentHill2.WidescreenFix")
 project "SilentHill3.WidescreenFix"
    setpaths("Z:/WFP/Games/Silent Hill/Silent Hill 3/", "sh3.exe")
+   writeghaction("sh3", "SilentHill3.WidescreenFix")
 project "SilentHill4.WidescreenFix"
    setpaths("Z:/WFP/Games/Silent Hill/Silent Hill 4 The Room/", "Silent Hill 4.exe")
+   writeghaction("sh4", "SilentHill4.WidescreenFix")
 project "SniperElite.WidescreenFix"
    setpaths("Z:/WFP/Games/Sniper Elite/", "SniperElite.exe")
+   writeghaction("sniperelite", "SniperElite.WidescreenFix")
 project "SonicHeroes.WidescreenFix"
    setpaths("Z:/WFP/Games/SONICHEROES/", "Tsonic_win.exe")
+   writeghaction("sonicheroes", "SonicHeroes.WidescreenFix")
 project "SplinterCell.WidescreenFix"
    setpaths("Z:/WFP/Games/Splinter Cell/Splinter Cell/", "system/SplinterCell.exe", "system/scripts/")
+   writeghaction("sc", "SplinterCell.WidescreenFix")
 project "SplinterCellChaosTheory.WidescreenFix"
    setpaths("Z:/WFP/Games/Splinter Cell/Splintercell Chaos Theory/", "system/splintercell3.exe", "system/scripts/")
+   writeghaction("scct", "SplinterCellChaosTheory.WidescreenFix")
 project "SplinterCellDoubleAgent.WidescreenFix"
    setpaths("Z:/WFP/Games/Splinter Cell/Splinter Cell - Double Agent/", "SCDA-Offline/System/SplinterCell4.exe", "SCDA-Offline/System/scripts/")
    files { "textures/SCDA/icon.rc" }
    defines { "IDR_SCDAICON=200" }
+   writeghaction("scda", "SplinterCellDoubleAgent.WidescreenFix")
 project "SplinterCellDoubleAgent.PCSX2.WidescreenFix"
    configurations { "ReleasePCSX2", "DebugPCSX2" }
       setpaths("Z:/WFP/Games/PCSX2/", "pcsx2.exe")
       files { "includes/pcsx2/pcsx2.h" }
+   writeghaction("scdaps2", "SplinterCellDoubleAgent.PCSX2.WidescreenFix")
 project "SplinterCellDoubleAgent.CXBXR.WidescreenFix"
    configurations { "ReleaseCXBXR", "DebugCXBXR" }
       setpaths("Z:/WFP/Games/CXBXR/", "cxbx.exe")
       files { "includes/cxbxr/cxbxr.h" }
+   writeghaction("scdaxbox", "SplinterCellDoubleAgent.CXBXR.WidescreenFix")
 project "SplinterCellPandoraTomorrow.WidescreenFix"
    setpaths("Z:/WFP/Games/Splinter Cell/Splinter Cell Pandora Tomorrow/", "offline/system/SplinterCell2.exe", "offline/system/scripts/")
+   writeghaction("scpt", "SplinterCellPandoraTomorrow.WidescreenFix")
 project "StreetRacingSyndicate.WidescreenFix"
    setpaths("Z:/WFP/Games/Street Racing Syndicate/", "Bin/srs.exe", "Bin/scripts/")
+   writeghaction("srs", "StreetRacingSyndicate.WidescreenFix")
 project "StubbstheZombie.WidescreenFix"
    setpaths("Z:/WFP/Games/Stubbs the Zombie/", "Stubbs.exe")
+   writeghaction("stubbsthezombie", "StubbstheZombie.WidescreenFix")
 project "TheGodfather.WidescreenFix"
    setpaths("Z:/WFP/Games/The Godfather/", "godfather.exe")
+   writeghaction("thegodfather", "TheGodfather.WidescreenFix")
 project "TheMatrixPathOfNeo.WidescreenFix"
    setpaths("Z:/WFP/Games/The Matrix - Path Of Neo/", "Matrix3.exe")
+   writeghaction("thematrixpathofneo", "TheMatrixPathOfNeo.WidescreenFix")
 project "ThePunisher.WidescreenFix"
    setpaths("Z:/WFP/Games/The Punisher/", "pun.exe")
+   writeghaction("punisher", "ThePunisher.WidescreenFix")
 project "TheSuffering.WidescreenFix"
    setpaths("Z:/WFP/Games/The Suffering/The Suffering/", "suffering.exe")
+   writeghaction("thesuffering", "TheSuffering.WidescreenFix")
 project "TonyHawksAmericanWasteland.WidescreenFix"
    setpaths("Z:/WFP/Games/Tony Hawks/Tony Hawk's American Wasteland/", "Game/THAW.exe", "Game/scripts/")
+   writeghaction("thaw", "TonyHawksAmericanWasteland.WidescreenFix")
 project "TonyHawksProSkater2.WidescreenFix"
    setpaths("Z:/WFP/Games/Tony Hawks/THPS2/", "THawk2.exe")
+   writeghaction("thps2", "TonyHawksProSkater2.WidescreenFix")
 project "TonyHawksProSkater3.WidescreenFix"
    setpaths("Z:/WFP/Games/Tony Hawks/Pro Skater 3/", "Skate3.exe")
+   writeghaction("thps3", "TonyHawksProSkater3.WidescreenFix")
 project "TonyHawksProSkater4.WidescreenFix"
    setpaths("Z:/WFP/Games/Tony Hawks/Tony Hawks Pro Skater 4/", "Game/Skate4.exe", "Game/scripts/")
+   writeghaction("thps4", "TonyHawksProSkater4.WidescreenFix")
 project "TonyHawksUnderground.WidescreenFix"
    setpaths("Z:/WFP/Games/Tony Hawks/Tony Hawk's Underground/", "Game/THUG.exe", "Game/scripts/")
+   writeghaction("thug", "TonyHawksUnderground.WidescreenFix")
 project "TonyHawksUnderground2.WidescreenFix"
    setpaths("Z:/WFP/Games/Tony Hawks/Tony Hawk's Underground 2/", "Game/THUG2.exe", "Game/scripts/")
+   writeghaction("thug2", "TonyHawksUnderground2.WidescreenFix")
 project "TotalOverdose.WidescreenFix"
    setpaths("Z:/WFP/Games/Total Overdose/", "TOD.exe")
+   writeghaction("tod", "TotalOverdose.WidescreenFix")
 project "TrueCrimeNewYorkCity.WidescreenFix"
    setpaths("Z:/WFP/Games/True Crime New York City/", "True Crime New York City.exe")
+   writeghaction("truecrimenyc", "TrueCrimeNewYorkCity.WidescreenFix")
 project "TrueCrimeStreetsofLA.WidescreenFix"
    setpaths("Z:/WFP/Games/True Crime Streets of LA/", "TrueCrimeMB.exe")
+   writeghaction("truecrimesola", "TrueCrimeStreetsofLA.WidescreenFix")
 project "UltimateSpiderMan.WidescreenFix"
    setpaths("Z:/WFP/Games/Ultimate Spider-Man/", "USM.exe")
+   writeghaction("usm", "UltimateSpiderMan.WidescreenFix")   
