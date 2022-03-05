257a63d03a8c778e507e508faf9ce7f94ddfd4a3
Sergey P <thirteenag@gmail.com>
2022-03-05 19:31:23 +0300

    Project2DFX for LCS PSP

--- premake5.lua
+++ premake5.lua
@@ -278,7 +278,7 @@ project "GTALCS.PPSSPP.WidescreenFix"
    files { "source/%{prj.name}/*.c" }
    targetextension ".prx"
    setbuildpaths_psp("Z:/WFP/Games/PPSSPP/", "PPSSPPWindows64.exe", "memstick/PSP/PLUGINS/GTALCS.PPSSPP.WidescreenFix/", "%{wks.location}/../external/pspsdk/bin/vsmake", "%{wks.location}/../source/%{prj.name}/", "GTALCS.PPSSPP.WidescreenFix")
-   writemakefile("GTALCS.PPSSPP.WidescreenFix")
+   writemakefile("GTALCS.PPSSPP.WidescreenFix", "lodl.c")
    writeghaction("gtavcspsp", "GTALCS.PPSSPP.WidescreenFix")
 project "GTAVCS.PPSSPP.WidescreenFix"
    kind "Makefile"
