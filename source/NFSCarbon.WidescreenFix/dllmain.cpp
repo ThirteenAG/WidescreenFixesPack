6963168d289922cba5a59821ba05fbbc5555c514
xan1242 <8014093+xan1242@users.noreply.github.com>
2023-07-03 04:24:47 +0200

    [NFSC] implement contrail limiter from ProStreet plugin

--- source/NFSCarbon.WidescreenFix/dllmain.cpp
+++ source/NFSCarbon.WidescreenFix/dllmain.cpp
@@ -141,6 +141,46 @@ void __declspec(naked) LightingFixUpdateMirrorCave()
     }
 }
 
+namespace ContrailHook
+{
+    float *fContrailSpeed;
+    bool bLimitContrailRate;
+    float fContrailTargetFPS;
+    float fContrailMinIntensity;
+    float fContrailMaxIntensity;
+
+    uintptr_t AddXenonEffect = 0x00754CD0;
+
+    uint32_t ContrailFrameDelay = 1;
+
+    uint32_t ContrailFC = 0;
+    uint32_t RenderConnFC = 0;
+    void AddXenonEffect_Contrail_Hook(void* piggyback_fx, void* spec, bMatrix4* mat, bVector4* vel, float intensity)
+    {
+        float newintensity = fContrailMaxIntensity;
+
+        double carspeed = ((sqrt((*vel).x * (*vel).x + (*vel).y * (*vel).y + (*vel).z * (*vel).z) - *fContrailSpeed)) / *fContrailSpeed;
+        newintensity = std::lerp(fContrailMinIntensity, fContrailMaxIntensity, carspeed);
+        if (newintensity > fContrailMaxIntensity)
+            newintensity = fContrailMaxIntensity;
+
+
+        if (!bLimitContrailRate)
+            return reinterpret_cast<void(*)(void*, void*, bMatrix4*, bVector4*, float)>(AddXenonEffect)(piggyback_fx, spec, mat, vel, newintensity);
+
+        // TODO: this could use actual timers instead of a simple framecounter to make it better, but it is good enough for this purpose
+        if ((ContrailFC + ContrailFrameDelay) <= RenderConnFC)
+        {
+            if (ContrailFC != RenderConnFC)
+            {
+                ContrailFC = RenderConnFC;
+                reinterpret_cast<void(*)(void*, void*, bMatrix4*, bVector4*, float)>(AddXenonEffect)(piggyback_fx, spec, mat, vel, newintensity);
+            }
+        }
+        RenderConnFC++;
+    }
+}
+
 #pragma runtime_checks( "", restore )
 
 void Init()
@@ -173,6 +213,12 @@ void Init()
     bool bDisableMotionBlur = iniReader.ReadInteger("GRAPHICS", "DisableMotionBlur", 0) != 0;
     bool bDisableContrails = iniReader.ReadInteger("GRAPHICS", "DisableContrails", 0) != 0;
     bool bFixXenonEffects = iniReader.ReadInteger("GRAPHICS", "FixXenonEffects", 1) != 0;
+
+    float cfgContrailSpeed = iniReader.ReadFloat("Contrails", "ContrailSpeed", 35.0f);
+    ContrailHook::bLimitContrailRate = iniReader.ReadInteger("Contrails", "LimitContrailRate", 1) != 0;
+    ContrailHook::fContrailTargetFPS = iniReader.ReadFloat("Contrails", "ContrailTargetFPS", 30.0f);
+    ContrailHook::fContrailMinIntensity = iniReader.ReadFloat("Contrails", "ContrailMinIntensity", 0.1f);
+    ContrailHook::fContrailMaxIntensity = iniReader.ReadFloat("Contrails", "ContrailMaxIntensity", 0.75f);
 
     bool bFixNOSTrailLength = iniReader.ReadInteger("NOSTrail", "FixNOSTrailLength", 1) == 1;
     bool bFixNOSTrailPosition = iniReader.ReadInteger("NOSTrail", "FixNOSTrailPosition", 0) != 0;
@@ -507,14 +553,6 @@ void Init()
     {
         uint32_t* dword_71356B = hook::pattern("D9 87 B4 00 00 00 D8 1D ? ? ? ? DF E0 F6 C4 41 75 76").count(1).get(0).get<uint32_t>(17); //0x0071355A
         injector::WriteMemory<uint8_t>(dword_71356B, 0xEB, true);
-    }
-
-    if (bDisableContrails)
-    {
-        uintptr_t loc_7E1351 = reinterpret_cast<uintptr_t>(hook::pattern("8A 87 9C 01 00 00 84 C0").get_first(0)) + 0xD0;
-        uintptr_t loc_7E13A9 = loc_7E1351 + 0x58;
-
-        injector::MakeJMP(loc_7E1351, loc_7E13A9, true);
     }
 
     if (bFixXenonEffects)
@@ -902,6 +940,37 @@ void Init()
 
         static float WorldMapConst4 = ((*(float*)dword_58F7B5) / (1 / TargetFPS)) * FrameTime;
         injector::WriteMemory(dword_58F7B5, &WorldMapConst4, true);
+    }
+
+
+    if (bDisableContrails)
+    {
+        uintptr_t loc_7E1351 = reinterpret_cast<uintptr_t>(hook::pattern("8A 87 9C 01 00 00 84 C0").get_first(0)) + 0xD0;
+        uintptr_t loc_7E13A9 = loc_7E1351 + 0x58;
+
+        injector::MakeJMP(loc_7E1351, loc_7E13A9, true);
+    }
+    else if (ContrailHook::bLimitContrailRate && (ContrailHook::fContrailTargetFPS > 0.0f))
+    {
+        uintptr_t loc_6B4D30 = reinterpret_cast<uintptr_t>(hook::pattern("D9 05 ? ? ? ? B9 ? ? ? ? D8 44 24 14 D9 5C 24 14").get_first(0)) + 0x35;
+        static float fGameTargetFPS = 1.0f / **reinterpret_cast<float**>(loc_6B4D30);
+
+        if (fGameTargetFPS != ContrailHook::fContrailTargetFPS)
+        {
+            // TODO: use a timer instead of this because of rounding errors!
+            float fContrailFrameDelay = (fGameTargetFPS / ContrailHook::fContrailTargetFPS);
+            ContrailHook::ContrailFrameDelay = static_cast<uint32_t>(round(fContrailFrameDelay));
+
+            uintptr_t loc_7E139A = reinterpret_cast<uintptr_t>(hook::pattern("8A 87 9C 01 00 00 84 C0").get_first(0)) + 0x119;
+            ContrailHook::AddXenonEffect = static_cast<uintptr_t>(injector::GetBranchDestination(loc_7E139A));
+            injector::MakeCALL(loc_7E139A, ContrailHook::AddXenonEffect_Contrail_Hook, true);
+
+            uintptr_t loc_7E1364 = reinterpret_cast<uintptr_t>(hook::pattern("8A 87 9C 01 00 00 84 C0").get_first(0)) + 0xE3;
+            ContrailHook::fContrailSpeed = *reinterpret_cast<float**>(loc_7E1364 + 2);
+            DWORD oldprotect;
+            injector::UnprotectMemory(ContrailHook::fContrailSpeed, sizeof(float), oldprotect);
+            *ContrailHook::fContrailSpeed = cfgContrailSpeed;
+        }
     }
 
     if (bFixNOSTrailLength)
