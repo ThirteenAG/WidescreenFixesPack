3a2e3a760cc427786bd9171cc90a3fbf44a8c590
ThirteenAG <thirteenag@gmail.com>
2023-08-30 23:48:17 +0800

    lcs psp fix
    - fixes #1431

--- source/GTALCS.PPSSPP.WidescreenFix/main.c
+++ source/GTALCS.PPSSPP.WidescreenFix/main.c
@@ -419,10 +419,12 @@ int OnModuleStart() {
         injector.MakeNOP(ptr + 0x74);
         
         // Force L trigger value in the L+camera movement function
-        ptr = pattern.get_first("0A 00 0A 85", 0);
-        injector.WriteInstr(ptr + 0x00,
-            li(t2, -0x1)
-        );
+        //ptr = pattern.get_first("0A 00 0A 85", 0);
+        //injector.WriteInstr(ptr + 0x00,
+        //    li(t2, -0x1)
+        //);
+        ptr = pattern.get(0, "02 00 04 34 25 30 80 00", -4);
+        injector.MakeNOP(ptr + 0x00);
     }
 
     if (ModernControlScheme)
