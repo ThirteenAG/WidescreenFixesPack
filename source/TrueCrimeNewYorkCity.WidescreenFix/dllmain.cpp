563ec0efb7c834206b6f93a08aa316e600603b81
ThirteenAG <thirteenag@gmail.com>
2026-01-14 21:30:36 +0300

    tcnyc: add various fps limiting options

--- source/TrueCrimeNewYorkCity.WidescreenFix/dllmain.cpp
+++ source/TrueCrimeNewYorkCity.WidescreenFix/dllmain.cpp
@@ -1,4 +1,6 @@
 #include "stdafx.h"
+#include <mmsystem.h>
+#pragma comment(lib, "winmm.lib")
 
 import Speedhack;
 
@@ -18,6 +20,85 @@ struct Screen
 
 float fFpsLimit;
 
+int32_t nFrameLimitType;
+class FrameLimiter
+{
+public:
+    enum FPSLimitMode { FPS_NONE, FPS_REALTIME, FPS_ACCURATE };
+    FPSLimitMode mFPSLimitMode = FPS_NONE;
+private:
+    double TIME_Frequency = 0.0;
+    double TIME_Ticks = 0.0;
+    double TIME_Frametime = 0.0;
+    float  fFPSLimit = 0.0f;
+public:
+    void Init(FPSLimitMode mode, float fps_limit)
+    {
+        mFPSLimitMode = mode;
+        fFPSLimit = fps_limit;
+
+        LARGE_INTEGER frequency;
+        QueryPerformanceFrequency(&frequency);
+        static constexpr auto TICKS_PER_FRAME = 1;
+        auto TICKS_PER_SECOND = (TICKS_PER_FRAME * fFPSLimit);
+        if (mFPSLimitMode == FPS_ACCURATE)
+        {
+            TIME_Frametime = 1000.0 / (double)fFPSLimit;
+            TIME_Frequency = (double)frequency.QuadPart / 1000.0; // ticks are milliseconds
+        }
+        else // FPS_REALTIME
+        {
+            TIME_Frequency = (double)frequency.QuadPart / (double)TICKS_PER_SECOND; // ticks are 1/n frames (n = fFPSLimit)
+        }
+        Ticks();
+    }
+    DWORD Sync_RT()
+    {
+        DWORD lastTicks, currentTicks;
+        LARGE_INTEGER counter;
+        QueryRealPerformanceCounter(&counter);
+        lastTicks = (DWORD)TIME_Ticks;
+        TIME_Ticks = (double)counter.QuadPart / TIME_Frequency;
+        currentTicks = (DWORD)TIME_Ticks;
+
+        return (currentTicks > lastTicks) ? currentTicks - lastTicks : 0;
+    }
+    DWORD Sync_SLP()
+    {
+        LARGE_INTEGER counter;
+        QueryRealPerformanceCounter(&counter);
+        double millis_current = (double)counter.QuadPart / TIME_Frequency;
+        double millis_delta = millis_current - TIME_Ticks;
+        if (TIME_Frametime <= millis_delta)
+        {
+            TIME_Ticks = millis_current;
+            return 1;
+        }
+        else if (TIME_Frametime - millis_delta > 2.0) // > 2ms
+            Sleep(1); // Sleep for ~1ms
+        else
+            Sleep(0); // yield thread's time-slice (does not actually sleep)
+
+        return 0;
+    }
+    void Sync()
+    {
+        if (mFPSLimitMode == FPS_REALTIME)
+            while (!Sync_RT());
+        else if (mFPSLimitMode == FPS_ACCURATE)
+            while (!Sync_SLP());
+    }
+private:
+    void Ticks()
+    {
+        LARGE_INTEGER counter;
+        QueryRealPerformanceCounter(&counter);
+        TIME_Ticks = (double)counter.QuadPart / TIME_Frequency;
+    }
+};
+
+FrameLimiter FpsLimiter;
+
 int32_t nLanguage;
 int32_t __cdecl SetLanguage(LPCSTR lpValueName)
 {
@@ -27,7 +108,7 @@ int32_t __cdecl SetLanguage(LPCSTR lpValueName)
 SafetyHookInline shsub_648AC0 = {};
 void __cdecl sub_648AC0(int a1)
 {
-    return shsub_648AC0.unsafe_ccall(a1);
+    return shsub_648AC0.unsafe_ccall(nFrameLimitType ? 0 : a1);
 }
 
 float fSensitivityFactor = 1.0f;
@@ -85,7 +166,7 @@ void Init()
     static bool bFixHUD = iniReader.ReadInteger("MAIN", "FixHUD", 1) != 0;
     static bool bFixFOV = iniReader.ReadInteger("MAIN", "FixFOV", 1) != 0;
 
-    static bool bFixGameSpeed = iniReader.ReadInteger("FRAMELIMIT", "FixGameSpeed", 1) != 0;
+    nFrameLimitType = iniReader.ReadInteger("FRAMELIMIT", "FrameLimitType", 1);
     fFpsLimit = std::clamp(static_cast<float>(iniReader.ReadInteger("FRAMELIMIT", "FpsLimit", 30)), 30.0f, FLT_MAX);
     fGameSpeedFactor = 30.0f / fFpsLimit;
 
@@ -189,8 +270,24 @@ void Init()
 
     if (fFpsLimit)
     {
+        if (nFrameLimitType > 0)
+        {
+            auto mode = (nFrameLimitType == 2) ? FrameLimiter::FPSLimitMode::FPS_ACCURATE : FrameLimiter::FPSLimitMode::FPS_REALTIME;
+            if (mode == FrameLimiter::FPSLimitMode::FPS_ACCURATE)
+                timeBeginPeriod(1);
+
+            FpsLimiter.Init(mode, fFpsLimit);
+        }
+
         pattern = hook::pattern("A1 ? ? ? ? 83 EC 1C");
         shsub_648AC0 = safetyhook::create_inline(pattern.get_first(0), sub_648AC0);
+
+        pattern = hook::pattern("8B 76 ? 8B 16 53");
+        static auto FPSLimiterPresent = safetyhook::create_mid(pattern.get_first(), [](SafetyHookContext& regs)
+        {
+            if (nFrameLimitType > 0 && fFpsLimit)
+                FpsLimiter.Sync();
+        });
 
         pattern = hook::pattern("A1 ? ? ? ? 83 C0 01 A3 ? ? ? ? A1");
         sub_62B450 = (decltype(sub_62B450))pattern.get_first();
@@ -198,10 +295,7 @@ void Init()
         pattern = hook::pattern("56 8B 35 ? ? ? ? 57 8B 3D ? ? ? ? 8B FF");
         injector::MakeJMP(pattern.get_first(), Thread, true);
 
-        if (bFixGameSpeed)
-        {
-            InitSpeedhack();
-        }
+        InitSpeedhack();
     }
 
     if (fSensitivityFactor)
