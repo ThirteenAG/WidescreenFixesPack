9401862d0fe0fbff8ef9d2f566a5f1b9bcb9188d
CommandGenius <rvlcariati@gmail.com>
2026-09-21 14:44:59 -0400

    EndScene shit

--- source/TheSaboteur.FusionFix/dllmain.cpp
+++ source/TheSaboteur.FusionFix/dllmain.cpp
@@ -4,11 +4,32 @@ import ComVars;
 
 void Init()
 {
-    auto pattern = hook::pattern("8B 91 ? ? ? ? 50 FF D2 8B CE");
+    static bool bEndSceneEventFired = false;
+
+    auto pattern = hook::pattern("83 05 ? ? ? ? 01 F7 47 08 00 00 00 80 8B CE");
+    static auto BeforeRenderPassHook = safetyhook::create_mid(pattern.get_first(), [](SafetyHookContext& regs)
+    {
+        if (bEndSceneEventFired)
+            return;
+
+        static constexpr std::string_view UIPasses[] = { "ClamberingHUDDrawList", "ClamberingHUDBucket", "Flash Bucket", "Debug" };
+        auto szPassName = (const char*)(regs.edi + 0x0C);
+        if (std::find(std::begin(UIPasses), std::end(UIPasses), szPassName) == std::end(UIPasses))
+            return;
+
+        static constexpr auto nDeviceOffset = 24424;
+        Direct3DDevice = *(IDirect3DDevice9**)(regs.esi + nDeviceOffset);
+        bEndSceneEventFired = true;
+        WFP::onEndScene().executeAll();
+    });
+
+    pattern = hook::pattern("8B 91 ? ? ? ? 50 FF D2 8B CE");
     static auto BeforeEndSceneHook = safetyhook::create_mid(pattern.get_first(), [](SafetyHookContext& regs)
     {
         Direct3DDevice = (IDirect3DDevice9*)regs.eax;
-        WFP::onEndScene().executeAll();
+        if (!bEndSceneEventFired)
+            WFP::onEndScene().executeAll();
+        bEndSceneEventFired = false;
     });
 
     static auto futures = WFP::onInitEventAsync().executeAllAsync();
